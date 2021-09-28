cbdf95dbc3f98dd9a8b9d925cc66b5c6caa9a2ab
Ben Yoshiwara <yoshiwara.b@gmail.com>
2021-09-28 12:17:07 -0700

    Simple gen art deployment script + typechain setup

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,17 @@
-require("@nomiclabs/hardhat-waffle");
-require('hardhat-contract-sizer');
-
+require("dotenv").config();
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-truffle5";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import "@nomiclabs/hardhat-solhint";
+import "hardhat-contract-sizer";
+import "@nomiclabs/hardhat-etherscan";
 
 const INFURA_PROJECT_ID = process.env.PROTOTYPE_BR_INFURA_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const MNEMONIC = process.env.MNEMONIC;
 const MAINNET_KEY = process.env.MAINNET_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -33,10 +39,14 @@ module.exports = {
       accounts: [`0x${MAINNET_KEY}`],
       gasPrice: 75000000000
     },
+    // ropsten: {
+    //   url: `http://127.0.0.1:8545`,
+    //   chainId:3,
+    //   from: `0x8De4e517A6F0B84654625228D8293b70AB49cF6C`
+    // },
     ropsten: {
-      url: `http://127.0.0.1:8545`,
-      chainId:3,
-      from: `0x8De4e517A6F0B84654625228D8293b70AB49cF6C`
+      url: process.env.ROPSTEN_JSON_RPC_PROVIDER_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
     },
     rinkeby: {
       url: `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`,
@@ -51,6 +61,9 @@ module.exports = {
       url: 'http://localhost:8545',
       // gasPrice: 8000000000, // 8 gwei
     }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
   },
   contractSizer: {
     alphaSort: true,
