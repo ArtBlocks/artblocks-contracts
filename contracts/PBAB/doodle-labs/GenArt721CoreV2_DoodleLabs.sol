4757aa740be32a08b3bc1cb503051b72b1314a86
Jacob Rockland <jakerockland@gmail.com>
2021-09-30 18:22:47 -0600

    Undo breakage of imports for DoodleLabs contract

--- contracts/PBAB/doodle-labs/GenArt721CoreV2_DoodleLabs.sol
+++ contracts/PBAB/doodle-labs/GenArt721CoreV2_DoodleLabs.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.5.0;
 
-import "./libs/CustomERC721Metadata.sol";
-import "./libs/SafeMath.sol";
-import "./libs/Strings.sol";
+import "../../libs/CustomERC721Metadata.sol";
+import "../../libs/SafeMath.sol";
+import "../../libs/Strings.sol";
 
 interface IRandomizer {
    function returnValue() external view returns(bytes32);
