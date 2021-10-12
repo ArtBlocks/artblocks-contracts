e1d9e612551316d21a6118ede4474dfdc2dd4ed9
Jake Rockland <jake@artblocks.io>
2021-10-11 19:30:39 -0600

    Add PBAB smart contracts for Flamingo/Flutter partnership. (#27)
    * Add branched smart contracts for Flutter/Flamingo project
    
    * For the deployment scripts for the Flutter smart contracts
    
    * Minor fixes to deployment scripts, and updated final deployment addresses for Ropsten

---
+++ scripts/PBAB/flutter/1_flutter_deploy_randomizer.ts
@@ -0,0 +1,17 @@
+import { ethers } from "hardhat";
+import { RandomizerFlutter__factory } from "../../contracts/factories/RandomizerFlutter__factory";
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  const randomizerFactory = new RandomizerFlutter__factory(deployer)
+  const randomizer = await randomizerFactory.deploy();
+  console.log(`Randomizer deployed at ${randomizer.address}`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
--- scripts/PBAB/flutter/2_flutter_deploy_coreV2.ts
+++ scripts/PBAB/flutter/2_flutter_deploy_coreV2.ts
@@ -1,13 +1,13 @@
 import { ethers } from "hardhat";
-import { GenArt721CoreV2DoodleLabs__factory } from "../../contracts/factories/GenArt721CoreV2DoodleLabs__factory";
+import { GenArt721CoreV2Flutter__factory } from "../../contracts/factories/GenArt721CoreV2Flutter__factory";
 
 async function main() {
   const [deployer] = await ethers.getSigners();
 
-  const genArt721CoreFactory = new GenArt721CoreV2DoodleLabs__factory(deployer);
+  const genArt721CoreFactory = new GenArt721CoreV2Flutter__factory(deployer);
   const genArt721Core = await genArt721CoreFactory.deploy(
-    "Doodle Labs Gen Art",
-    "DOODLE",
+    "Flutter Gen Art",
+    "FLUTTER",
     "TODO: Enter Randomizer address here."
   );
   console.log(`GenArt721Core deployed at ${genArt721Core.address}`);
