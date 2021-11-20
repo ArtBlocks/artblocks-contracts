8a6113d196d33a58235be7b82fb74951231a8068
Jake Rockland <jake@artblocks.io>
2021-11-19 17:22:17 -0700

    use modifier for core contract whitelist checking

--- contracts/MinterFilter.sol
+++ contracts/MinterFilter.sol
@@ -20,35 +20,38 @@ contract MinterFilter {
 
     mapping(uint256 => address) public minterForProject;
 
+    modifier onlyCoreWhitelisted() {
+        require(
+            artblocksContract.isWhitelisted(msg.sender),
+            "Only Core whitelisted"
+        );
+        _;
+    }
+
     constructor(address _genArt721Address) public {
         artblocksContract = IGenArt721CoreContract(_genArt721Address);
     }
 
     function setMinterForProject(uint256 _projectId, address _minterAddress)
         public
+        onlyCoreWhitelisted
     {
-        require(
-            artblocksContract.isWhitelisted(msg.sender),
-            "can only be set by admin"
-        );
         minterForProject[_projectId] = _minterAddress;
         emit ProjectMinterRegistered(_projectId, _minterAddress);
     }
 
-    function setDefaultMinter(address _minterAddress) public {
-        require(
-            artblocksContract.isWhitelisted(msg.sender),
-            "can only be set by admin"
-        );
+    function setDefaultMinter(address _minterAddress)
+        public
+        onlyCoreWhitelisted
+    {
         defaultMinter = _minterAddress;
         emit DefaultMinterRegistered(_minterAddress);
     }
 
-    function resetMinterForProjectToDefault(uint256 _projectId) public {
-        require(
-            artblocksContract.isWhitelisted(msg.sender),
-            "can only be set by admin"
-        );
+    function resetMinterForProjectToDefault(uint256 _projectId)
+        public
+        onlyCoreWhitelisted
+    {
         minterForProject[_projectId] = address(0);
         emit ProjectMinterRegistered(_projectId, address(0));
     }
