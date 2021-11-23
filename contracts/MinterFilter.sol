6950cf438e9251c7e1d89d6f9f2cca0694135684
Jake Rockland <jake@artblocks.io>
2021-11-22 19:09:32 -0700

    Minter filter improvements (#58)
    * refactor minterfilter to use interface oriented design
    
    * add event emmission to minterfilter
    
    * cleanup consistency within tests
    
    * Add unit tests for minterfilter event emmision
    
    * Add unit tests for minterfilter event emmision
    
    * Simplify event emmision
    
    * use modifier for core contract whitelist checking
    
    * Prefer external to public where applicable
    
    * make consistent use of address(0)
    
    * remove undeeded 0x0 address check
    
    * remove unused import

--- contracts/MinterFilter.sol
+++ contracts/MinterFilter.sol
@@ -1,5 +1,4 @@
 import "./libs/SafeMath.sol";
-import "./libs/Strings.sol";
 
 import "./interfaces/IGenArt721CoreContract.sol";
 
@@ -20,35 +19,38 @@ contract MinterFilter {
 
     mapping(uint256 => address) public minterForProject;
 
+    modifier onlyCoreWhitelisted() {
+        require(
+            artblocksContract.isWhitelisted(msg.sender),
+            "Only Core whitelisted"
+        );
+        _;
+    }
+
     constructor(address _genArt721Address) public {
         artblocksContract = IGenArt721CoreContract(_genArt721Address);
     }
 
     function setMinterForProject(uint256 _projectId, address _minterAddress)
-        public
+        external
+        onlyCoreWhitelisted
     {
-        require(
-            artblocksContract.isWhitelisted(msg.sender),
-            "can only be set by admin"
-        );
         minterForProject[_projectId] = _minterAddress;
         emit ProjectMinterRegistered(_projectId, _minterAddress);
     }
 
-    function setDefaultMinter(address _minterAddress) public {
-        require(
-            artblocksContract.isWhitelisted(msg.sender),
-            "can only be set by admin"
-        );
+    function setDefaultMinter(address _minterAddress)
+        external
+        onlyCoreWhitelisted
+    {
         defaultMinter = _minterAddress;
         emit DefaultMinterRegistered(_minterAddress);
     }
 
-    function resetMinterForProjectToDefault(uint256 _projectId) public {
-        require(
-            artblocksContract.isWhitelisted(msg.sender),
-            "can only be set by admin"
-        );
+    function resetMinterForProjectToDefault(uint256 _projectId)
+        external
+        onlyCoreWhitelisted
+    {
         minterForProject[_projectId] = address(0);
         emit ProjectMinterRegistered(_projectId, address(0));
     }
@@ -57,11 +59,10 @@ contract MinterFilter {
         address _to,
         uint256 _projectId,
         address sender
-    ) public returns (uint256 _tokenId) {
+    ) external returns (uint256 _tokenId) {
         require(
-            (minterForProject[_projectId] != address(0x0) &&
-                msg.sender == minterForProject[_projectId]) ||
-                (minterForProject[_projectId] == address(0x0) &&
+            (msg.sender == minterForProject[_projectId]) ||
+                (minterForProject[_projectId] == address(0) &&
                     msg.sender == defaultMinter),
             "Not sent from correct minter for project"
         );
