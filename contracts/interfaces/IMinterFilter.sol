942806097b5871e6aa75a1576392b518b7239519
ryley-o <30364988+ryley-o@users.noreply.github.com>
2021-11-22 23:02:37 -0800

    fix IMinterFilter to align w/actual minter filter. inherit from interface.

--- contracts/interfaces/IMinterFilter.sol
+++ contracts/interfaces/IMinterFilter.sol
@@ -1,12 +1,12 @@
 pragma solidity ^0.5.0;
 
 interface IMinterFilter {
-    function setOwnerAddress(address payable _ownerAddress) external;
-
-    function setMinterForProject(uint256 _projectId, address _minterAddress)
+    function setMinterForProject(uint256, address)
         external;
 
-    function disableMinterForProject(uint256 _projectId) external;
+    function setDefaultMinter(address) external;
+
+    function resetMinterForProjectToDefault(uint256) external;
 
     function mint(
         address _to,
