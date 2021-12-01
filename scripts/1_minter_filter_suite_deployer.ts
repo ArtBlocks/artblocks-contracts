afac63e6d769dc3781391951cbaf6cbd4da117d3
Jake Rockland <jake@artblocks.io>
2021-12-01 12:52:59 -0700

    fix network reference error

--- scripts/1_minter_filter_suite_deployer.ts
+++ scripts/1_minter_filter_suite_deployer.ts
@@ -88,6 +88,7 @@ async function main() {
   );
 
   // Output instructions for manual Etherscan verification.
+  const network = await ethers.provider.getNetwork();
   const networkName = network.name == "homestead" ? "mainnet" : network.name;
   const standardVerify =
     "yarn hardhat verify --contract <path to .sol>:<contract name>";
