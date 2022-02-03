9859e11dfc24ccd1c6a4790eb9b109dc2554ae0c
ryley-o <30364988+ryley-o@users.noreply.github.com>
2022-02-02 21:19:34 -0800

    update tests to test MinterFilter with V3 core instead of legacy minter with V3 core

--- test/GenArt721CoreV3_Integration.test.ts
+++ test/GenArt721CoreV3_Integration.test.ts
@@ -35,22 +35,37 @@ describe("GenArt721CoreV3", async function () {
     this.token = await artblocksFactory
       .connect(snowfro)
       .deploy(name, symbol, this.randomizer.address);
+    // deploy and configure minter filter and minter
+    const minterFilterFactory = await ethers.getContractFactory("MinterFilter");
+    this.minterFilter = await minterFilterFactory.deploy(this.token.address);
     const minterFactory = await ethers.getContractFactory(
-      "GenArt721LegacyMinter"
+      "GenArt721FilteredMinterETH"
     );
-    this.minter = await minterFactory.deploy(this.token.address);
-
+    this.minter = await minterFactory.deploy(
+      this.token.address,
+      this.minterFilter.address
+    );
+    await this.minterFilter
+      .connect(snowfro)
+      .addApprovedMinter(this.minter.address);
     await this.token
       .connect(snowfro)
-      .addProject("name", artist.address, pricePerTokenInWei);
-
-    this.projectZeroInfo = await this.token.projectTokenInfo(projectZero);
-
+      .updateMinterContract(this.minterFilter.address);
+    // add project
+    await this.token.connect(snowfro).addProject("name", artist.address);
     await this.token.connect(snowfro).toggleProjectIsActive(projectZero);
-    await this.token.connect(snowfro).updateMinterContract(this.minter.address);
     await this.token
       .connect(artist)
       .updateProjectMaxInvocations(projectZero, 15);
+    // set project's minter and price
+    await this.minter
+      .connect(artist)
+      .updatePricePerTokenInWei(projectZero, pricePerTokenInWei);
+    await this.minterFilter
+      .connect(artist)
+      .setMinterForProject(projectZero, this.minter.address);
+    // get project's info
+    this.projectZeroInfo = await this.token.projectTokenInfo(projectZero);
   });
 
   describe("has whitelisted owner", function () {
