8566d20d1f77936ea0629446169588a7f35beef3
Michael Chrupcala <michael@artblocks.io>
2022-02-09 14:28:06 -0500

    PBAB royalty deploy script is successful

--- scripts/1_royalty_registry_PBAB_deployer.ts
+++ scripts/1_royalty_registry_PBAB_deployer.ts
@@ -4,19 +4,22 @@
 import { ethers } from "hardhat";
 import { GenArt721RoyaltyOverridePBAB__factory, Randomizer__factory, GenArt721CoreV2PBAB__factory, GenArt721MinterPBAB__factory } from "./contracts";
 import royaltyRegistryABI from "../contracts/libs/abi/RoyaltyRegistry.json";
-// import genArt721CoreV3RegistryABI from "../artifacts/contracts/GenArt721CoreV3.sol/GenArt721CoreV3.json";
+import royaltyEngineV1ABI from "../contracts/libs/abi/RoyaltyEngineV1.json";
 
 async function main() {
   const [deployer] = await ethers.getSigners();
   const network = await ethers.provider.getNetwork();
 
-  function getRoyaltyRegistryAddress(networkName: string): string {
+  function getRoyaltyAddress(networkName: string, contract: string): string {
     // ref: https://royaltyregistry.xyz/lookup)
     if (networkName == "ropsten") {
-      return "0x9cac159ec266E76ed7377b801f3b5d2cC7bcf40d";
+      return contract === 'registry' ? "0x9cac159ec266E76ed7377b801f3b5d2cC7bcf40d" : "0xFf5A6F7f36764aAD301B7C9E85A5277614Df5E26"
+    }
+    if (networkName == "rinkeby") {
+      return contract === 'registry' ? "0xc9198CbbB57708CF31e0caBCe963c98e60d333c3" : "0x8d17687ea9a6bb6efA24ec11DcFab01661b2ddcd"
     }
     if (networkName == "mainnet") {
-      return "0xaD2184FB5DBcfC05d8f056542fB25b04fa32A95D";
+      return contract === 'registry' ? "0xaD2184FB5DBcfC05d8f056542fB25b04fa32A95D" : "0X0385603AB55642CB4DD5DE3AE9E306809991804F"
     }
     return;
   }
@@ -32,10 +35,15 @@ async function main() {
   }
 
   // GET ROYALTY REGISTRY CONTRACT TO SET OVERRIDE ADDRESS
-  const RoyaltyRegistryAddress = getRoyaltyRegistryAddress(network.name);
+  const RoyaltyRegistryAddress = getRoyaltyAddress(network.name, 'registry');
+  const RoyaltyEngineAddress = getRoyaltyAddress(network.name, 'engine');
   const RoyaltyRegistryContract = await ethers.getContractAt(
     royaltyRegistryABI,
     RoyaltyRegistryAddress
+  );
+  const RoyaltyEngineContract = await ethers.getContractAt(
+    royaltyEngineV1ABI,
+    RoyaltyEngineAddress
   );
 
   //////////////////////////////////////////////////////////////////////////////
@@ -58,9 +66,6 @@ async function main() {
 
   const tokenNamePBAB = "Token Placeholder";
   const tokenTickerPBAB = "TOKN";
-  const artistAddressPBAB = "0x18579FBCe9152A1342663678FA970DcA4AE654fF";
-  const artblocksRoyaltiesAddressPBAB = "0xdddD4e84E9B742236543Bc497F0b3fd2a47f34D8"
-  const pbabTransferAddress = "0x451d3de659e8fc81054F07C37e0e889E1eE6B3b5";     // new PBAB owner
   const rendererProviderAddress = "0x1ea6EFF00AB9E00160214e641A24FCa30A83DF87"; // dummy AB royalties address
   const platformRoyaltyPaymentAddress = "0x4Ee9e3fc2Cb7bE2daF4e91f1b9b9F5DB3F225d53";
 
@@ -88,28 +93,26 @@ async function main() {
   // Allowlist the Minter on the Core contract.
   await coreTokenPBAB
     .connect(deployer)
-    .addMintWhitelisted(genArt721MinterPBAB.address);
+    .addMintWhitelisted(genArt721MinterPBAB.address, { gasLimit: 500000 });
   console.log(`Allowlisted the Minter on the Core contract.`);
 
   // Update the Renderer provider.
   await coreTokenPBAB
     .connect(deployer)
-    .updateRenderProviderAddress(rendererProviderAddress);
+    .updateRenderProviderAddress(rendererProviderAddress, { gasLimit: 500000 });
   console.log(`Updated the renderer provider to: ${rendererProviderAddress}.`);
 
-  await coreTokenPBAB.connect(deployer).addProject("test_project", artistAddressPBAB, ethers.utils.parseEther("0.10"));
-  await coreTokenPBAB.connect(deployer).updateProjectSecondaryMarketRoyaltyPercentage(0, 5);
-  await coreTokenPBAB.connect(deployer).updateArtblocksRoyaltyAddressForContract()
-  await coreTokenPBAB.connect(deployer).updateProjectAdditionalPayeeInfo(0, "0x24CdEE54439D3f25DE4cddE43e41B19FdAA90cE8", 20);
+  await coreTokenPBAB.connect(deployer).addProject("test_project", deployer.address, ethers.utils.parseEther("0.10"), { gasLimit: 500000 });
+  await coreTokenPBAB.connect(deployer).updateProjectSecondaryMarketRoyaltyPercentage(0, 5, { gasLimit: 500000 });
+  await coreTokenPBAB.connect(deployer).updateProjectAdditionalPayeeInfo(0, "0x24CdEE54439D3f25DE4cddE43e41B19FdAA90cE8", 20, { gasLimit: 500000 });
 
-  await coreTokenPBAB.connect(deployer).toggleProjectIsActive(0);
+  await coreTokenPBAB.connect(deployer).toggleProjectIsActive(0, { gasLimit: 500000 });
 
-  await coreTokenPBAB.connect(deployer).addMintWhitelisted(this.minterFilterA.address);
+  await coreTokenPBAB.connect(deployer).updateProjectMaxInvocations(0, 15, { gasLimit: 500000 });
 
-  await coreTokenPBAB.connect(deployer).updateProjectMaxInvocations(0, 15);
+  await coreTokenPBAB.connect(deployer).toggleProjectIsPaused(0, { gasLimit: 500000 });
 
-  coreTokenPBAB.connect(artistAddressPBAB).toggleProjectIsPaused(0);
-
+  const sleep = ms => new Promise(r => setTimeout(r, ms));
 
   
   ///////////////////////////////////////
@@ -123,7 +126,8 @@ async function main() {
   //SETS THE MANIFOLD REGISTRY TO LOOKUP AB ROYALTY OVERRIDE FOR AB CORE
   await RoyaltyRegistryContract.connect(deployer).setRoyaltyLookupAddress(
     coreTokenPBAB.address, // token address
-    overridePBAB.address // royalty override address
+    overridePBAB.address, // royalty override address
+    { gasLimit: 500000 }
   );
   console.log(
     `Royalty Registry override for new GenArt721Core set to: ` +
@@ -136,7 +140,8 @@ async function main() {
       deployer
     ).updatePlatformRoyaltyAddressForContract(
       coreTokenPBAB.address, // token address
-      platformRoyaltyPaymentAddress // platform royalty payment address
+      platformRoyaltyPaymentAddress, // platform royalty payment address
+      { gasLimit: 500000 }
     );
     console.log(
       `Platform Royalty Payment Address for newly deployed GenArt721Core ` +
@@ -144,11 +149,15 @@ async function main() {
         `override contract at ${overridePBAB.address})`
     );
 
-    await genArt721MinterPBAB.connect(deployer).purchase(0, { value: ethers.utils.parseEther("0.10") });
+    const token = await genArt721MinterPBAB.connect(deployer).purchase(0, { value: ethers.utils.parseEther("0.10"), gasLimit: 50000 });
+
+    console.log('Minted token: ', token)
+
+    await sleep(20000);
 
       //VERIFY THAT IT WORKED
     //call RoyaltyRegistryEngine --> check for royalties result. should give me BPS and addresses
-  const res = await RoyaltyRegistryContract.connect(deployer).getRoyaltiesView();
+  const res = await RoyaltyEngineContract.connect(deployer).getRoyaltyView(coreTokenPBAB.address, 0, ethers.utils.parseEther("1"), { gasLimit: 500000 });
 
   console.log(res);
 }
