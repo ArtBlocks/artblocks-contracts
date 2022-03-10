7343d7345996f4d4dbef693fe5c2a84b30629a66
Ben Yoshiwara <yoshiwara.b@gmail.com>
2022-03-10 10:12:35 -0800

    Update contracts/minter-suite/MinterDALinV0.sol
    Co-authored-by: Jake Rockland <jake@artblocks.io>

--- contracts/minter-suite/MinterDALinV0.sol
+++ contracts/minter-suite/MinterDALinV0.sol
@@ -42,7 +42,7 @@ contract MinterDALinV0 is ReentrancyGuard, IFilteredMinterV0 {
     address public immutable minterFilterAddress;
 
     /// Minter filter this minter may interact with.
-    IMinterFilterV0 public immutable minterFilter;
+    IMinterFilterV0 private immutable minterFilter;
 
     /// minterType for this minter
     string public constant minterType = "MinterDALinV0";
