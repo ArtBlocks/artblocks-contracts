9c7e481f3c2464ccd8df3d055eaef2d1bd00a48d
Ben Yoshiwara <yoshiwara.b@gmail.com>
2022-03-10 10:12:39 -0800

    Update contracts/minter-suite/MinterSetPriceERC20V0.sol
    Co-authored-by: Jake Rockland <jake@artblocks.io>

--- contracts/minter-suite/MinterSetPriceERC20V0.sol
+++ contracts/minter-suite/MinterSetPriceERC20V0.sol
@@ -26,7 +26,7 @@ contract MinterSetPriceERC20V0 is ReentrancyGuard, IFilteredMinterV0 {
     address public immutable minterFilterAddress;
 
     /// Minter filter this minter may interact with.
-    IMinterFilterV0 public immutable minterFilter;
+    IMinterFilterV0 private immutable minterFilter;
 
     /// minterType for this minter
     string public constant minterType = "MinterSetPriceERC20V0";
