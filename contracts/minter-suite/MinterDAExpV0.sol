e2ac9bd00cdeeb8ade93f8ff031430b80a43c433
Ben Yoshiwara <yoshiwara.b@gmail.com>
2022-03-10 10:12:28 -0800

    Update contracts/minter-suite/MinterDAExpV0.sol
    Co-authored-by: Jake Rockland <jake@artblocks.io>

--- contracts/minter-suite/MinterDAExpV0.sol
+++ contracts/minter-suite/MinterDAExpV0.sol
@@ -43,7 +43,7 @@ contract MinterDAExpV0 is ReentrancyGuard, IFilteredMinterV0 {
     address public immutable minterFilterAddress;
 
     /// Minter filter this minter may interact with.
-    IMinterFilterV0 public immutable minterFilter;
+    IMinterFilterV0 private immutable minterFilter;
 
     /// minterType for this minter
     string public constant minterType = "MinterDAExpV0";
