832deba145b3071fc28c39e1de778a5cd7e6e9bf
hanaxmoto <rebekah.k@artblocks.io>
2022-04-05 16:50:16 -0600

    add aws v3 client mock, cdd unit tests, change test syntax from jest bdd to mocha assert

---
+++ test/util/AWSS3.test.ts
@@ -0,0 +1,55 @@
+import { assert } from "chai";
+const { mockClient } = require("aws-sdk-client-mock");
+const { S3Client, CreateBucketCommand } = require("@aws-sdk/client-s3");
+import { createPBABBuckets, createBucket } from "../../scripts/util/aws_s3";
+import { formatTitleCaseToKebabCase } from "../../scripts/util/format";
+
+// Mock client documentation: https://www.npmjs.com/package/aws-sdk-client-mock
+const s3ClientMock = mockClient(new S3Client({}));
+
+const createBucketResponse = {
+  Location: "/foobar-pbab-bucket",
+};
+
+const createPBABBucketsResponse = {
+  staging: {
+    Location: "/foobar-pbab-bucket-staging",
+  },
+  production: {
+    Location: "/foobar-pbab-bucket-mainnet",
+  },
+};
+
+describe("Create S3 Bucket for PBAB", () => {
+  beforeEach(() => {
+    s3ClientMock.reset();
+    s3ClientMock.on(CreateBucketCommand).resolves(createBucketResponse);
+  });
+
+  it("creates s3 bucket", async () => {
+    const pbabBucketName = "foobar-pbab-bucket";
+    const result = await createBucket(s3ClientMock as any, pbabBucketName);
+    assert(result !== null);
+    assert(result?.["Location"] === "/foobar-pbab-bucket");
+  });
+
+  it("creates s3 bucket for staging and prod", async () => {
+    const pbabTokenName = "Foobar PBAB Bucket";
+    const result = await createPBABBuckets(s3ClientMock as any, pbabTokenName);
+    assert(result !== null);
+    assert(result?.["staging"]["Location"] === createPBABBucketsResponse["staging"]["Location"]);
+    assert(
+      result?.["production"]["Location"] === createPBABBucketsResponse["production"]["Location"]
+    );
+  });
+});
+
+describe("Format bucket name", () => {
+  it("formats PBAB token name to hyphened lowercase", () => {
+    const pbabTokenName = "Foobar PBAB Bucket";
+    const bucketName = formatTitleCaseToKebabCase(pbabTokenName);
+    assert(bucketName === "foobar-pbab-bucket");
+  });
+});
+
+// TOOD: check usage of current bucket naming conventions to upload assets
--- test/util/AWSS3_integration.test.ts
+++ test/util/AWSS3_integration.test.ts
@@ -1,3 +1,4 @@
+import { assert } from "chai";
 const { S3Client, ListBucketsCommand } = require("@aws-sdk/client-s3");
 
 describe("Integration Test of AWS SDK Client to Create S3 Bucket", () => {
@@ -5,9 +6,9 @@ describe("Integration Test of AWS SDK Client to Create S3 Bucket", () => {
     const client = new S3Client({ region: "us-east-1" });
     const command = new ListBucketsCommand({});
     const response = await client.send(command);
-    expect(response).toBeDefined();
-    expect(response.$metadata.httpStatusCode).toEqual(200);
-    expect(response.Buckets?.length).toBeGreaterThan(0)
+    assert(response !== null);
+    assert(response.$metadata.httpStatusCode === 200);
+    expect(response.Buckets?.length >= 0);
     client.destroy();
   });
 });
