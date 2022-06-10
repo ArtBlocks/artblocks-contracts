20fc6d68699d8064b1d30158e4b995e256a3ec72
Jake Rockland <jake@artblocks.io>
2022-06-10 17:59:00 -0600

    Format

--- contracts/GenArt721CoreV3.sol
+++ contracts/GenArt721CoreV3.sol
@@ -633,16 +633,16 @@ contract GenArt721CoreV3 is ERC721Enumerable, IGenArt721CoreContractV3 {
         ];
         royaltyFeeByID = projectIdToSecondaryMarketRoyaltyPercentage[projectId];
     }
-    
+
     /**
      * @notice Gets the project ID for a given `_tokenId`.
      */
-    function tokenIdToProjectId(uint256 _tokenId) 
+    function tokenIdToProjectId(uint256 _tokenId)
         public
         view
         returns (uint256 _projectId)
     {
-        return _tokenId / ONE_MILLION
+        return _tokenId / ONE_MILLION;
     }
 
     /**
