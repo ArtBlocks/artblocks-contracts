4f33d1f5c8f3b39a79aa2c8a54ff70c2c77eb4fd
ryley-o <30364988+ryley-o@users.noreply.github.com>
2022-06-14 22:03:47 -0700

    fix expected gas in mint test

--- test/MinterHolderV0.test.ts
+++ test/MinterHolderV0.test.ts
@@ -667,7 +667,7 @@ describe("MinterHolderV0", async function () {
         ethers.utils.formatUnits(txCost, "ether").toString(),
         "ETH"
       );
-      expect(txCost.toString()).to.equal(ethers.utils.parseEther("0.0319919"));
+      expect(txCost.toString()).to.equal(ethers.utils.parseEther("0.0319931"));
     });
   });
 
