8b1a188ce75ca9aae189728658d63c65a0b7a81c
ryley-o <30364988+ryley-o@users.noreply.github.com>
2022-07-08 17:01:23 -0700

    update V3 core intergration to be passing

--- test/core/GenArt721CoreV3_Integration.test.ts
+++ test/core/GenArt721CoreV3_Integration.test.ts
@@ -9,83 +9,76 @@ import {
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
+import {
+  getAccounts,
+  assignDefaultConstants,
+  deployAndGet,
+  deployCoreWithMinterFilter,
+} from "../util/common";
+
 /**
  * NOTE: V3 core does not currently have a compatible minter.
  * Tests/operations involving minter currently commented out in this file -
  * to be updated when developing a minter for V3 core.
  */
 describe("GenArt721CoreV3", async function () {
-  const name = "Non Fungible Token";
-  const symbol = "NFT";
-
-  const firstTokenId = new BN("30000000");
-  const secondTokenId = new BN("3000001");
-
-  const pricePerTokenInWei = ethers.utils.parseEther("1");
-  const projectZero = 0;
-
   beforeEach(async function () {
-    const [owner, newOwner, artist, additional, snowfro] =
-      await ethers.getSigners();
-    this.accounts = {
-      owner: owner,
-      newOwner: newOwner,
-      artist: artist,
-      additional: additional,
-      snowfro: snowfro,
-    };
+    // standard accounts and constants
+    this.accounts = await getAccounts.call(this);
+    await assignDefaultConstants.call(this);
+
     const randomizerFactory = await ethers.getContractFactory(
       "BasicRandomizer"
     );
     this.randomizer = await randomizerFactory.deploy();
     const artblocksFactory = await ethers.getContractFactory("GenArt721CoreV3");
     this.genArt721Core = await artblocksFactory
-      .connect(snowfro)
-      .deploy(name, symbol, this.randomizer.address);
+      .connect(this.accounts.deployer)
+      .deploy(this.name, this.symbol, this.randomizer.address);
 
     // TBD - V3 DOES NOT CURRENTLY HAVE A WORKING MINTER
 
     // add project
     await this.genArt721Core
-      .connect(snowfro)
-      .addProject("name", artist.address);
+      .connect(this.accounts.deployer)
+      .addProject("name", this.accounts.artist.address);
     await this.genArt721Core
-      .connect(snowfro)
-      .toggleProjectIsActive(projectZero);
+      .connect(this.accounts.deployer)
+      .toggleProjectIsActive(this.projectZero);
     await this.genArt721Core
-      .connect(artist)
-      .updateProjectMaxInvocations(projectZero, 15);
+      .connect(this.accounts.artist)
+      .updateProjectMaxInvocations(this.projectZero, this.maxInvocations);
   });
 
   describe("has whitelisted owner", function () {
     it("has an admin", async function () {
       expect(await this.genArt721Core.artblocksAddress()).to.be.equal(
-        this.accounts.snowfro.address
+        this.accounts.deployer.address
       );
     });
 
     it("has an admin", async function () {
       expect(await this.genArt721Core.admin()).to.be.equal(
-        this.accounts.snowfro.address
+        this.accounts.deployer.address
       );
     });
 
     describe("has whitelisted owner", function () {
       it("has an admin", async function () {
         expect(await this.genArt721Core.artblocksAddress()).to.be.equal(
-          this.accounts.snowfro.address
+          this.accounts.deployer.address
         );
       });
 
       it("has an admin", async function () {
         expect(await this.genArt721Core.admin()).to.be.equal(
-          this.accounts.snowfro.address
+          this.accounts.deployer.address
         );
       });
 
       it("has a whitelisted account", async function () {
         expect(
-          await this.genArt721Core.isWhitelisted(this.accounts.snowfro.address)
+          await this.genArt721Core.isWhitelisted(this.accounts.deployer.address)
         ).to.be.equal(true);
       });
     });
@@ -93,12 +86,12 @@ describe("GenArt721CoreV3", async function () {
     describe("reverts on project locked", async function () {
       it("reverts if try to modify script", async function () {
         await this.genArt721Core
-          .connect(this.accounts.snowfro)
-          .toggleProjectIsLocked(projectZero);
+          .connect(this.accounts.deployer)
+          .toggleProjectIsLocked(this.projectZero);
         await expectRevert(
           this.genArt721Core
             .connect(this.accounts.artist)
-            .updateProjectScriptJSON(projectZero, "lorem ipsum"),
+            .updateProjectScriptJSON(this.projectZero, "lorem ipsum"),
           "Only if unlocked"
         );
       });
@@ -108,7 +101,7 @@ describe("GenArt721CoreV3", async function () {
   describe("projectInfo", function () {
     it("returns expected deprecated values", async function () {
       const tokenInfo = await this.genArt721Core
-        .connect(this.accounts.snowfro)
+        .connect(this.accounts.deployer)
         .projectInfo(0);
       expect(tokenInfo.invocations).to.be.equal(0);
       expect(tokenInfo.maxInvocations).to.be.equal(15);
