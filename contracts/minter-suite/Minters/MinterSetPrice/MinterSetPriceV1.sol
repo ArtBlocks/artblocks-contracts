bb1e10e7067e663eacd74d87d2d1a40bf2ddff61
ryley-o <30364988+ryley-o@users.noreply.github.com>
2022-08-01 21:47:49 -0700

    refactor minter directory structure

--- contracts/minter-suite/Minters/MinterSetPrice/MinterSetPriceV1.sol
+++ contracts/minter-suite/Minters/MinterSetPrice/MinterSetPriceV1.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 // Created By: Art Blocks Inc.
 
-import "../interfaces/0.8.x/IGenArt721CoreContractV1.sol";
-import "../interfaces/0.8.x/IMinterFilterV0.sol";
-import "../interfaces/0.8.x/IFilteredMinterV0.sol";
+import "../../../interfaces/0.8.x/IGenArt721CoreContractV1.sol";
+import "../../../interfaces/0.8.x/IMinterFilterV0.sol";
+import "../../../interfaces/0.8.x/IFilteredMinterV0.sol";
 
 import "@openzeppelin-4.5/contracts/security/ReentrancyGuard.sol";
 
@@ -11,27 +11,9 @@ pragma solidity 0.8.9;
 
 /**
  * @title Filtered Minter contract that allows tokens to be minted with ETH.
- * Pricing is achieved using an automated Dutch-auction mechanism.
  * @author Art Blocks Inc.
  */
-contract MinterDALinV1 is ReentrancyGuard, IFilteredMinterV0 {
-    /// Auction details updated for project `projectId`.
-    event SetAuctionDetails(
-        uint256 indexed projectId,
-        uint256 _auctionTimestampStart,
-        uint256 _auctionTimestampEnd,
-        uint256 _startPrice,
-        uint256 _basePrice
-    );
-
-    /// Auction details cleared for project `projectId`.
-    event ResetAuctionDetails(uint256 indexed projectId);
-
-    /// Minimum allowed auction length updated
-    event MinimumAuctionLengthSecondsUpdated(
-        uint256 _minimumAuctionLengthSeconds
-    );
-
+contract MinterSetPriceV1 is ReentrancyGuard, IFilteredMinterV0 {
     /// Core contract address this minter interacts with
     address public immutable genArt721CoreAddress;
 
@@ -45,7 +27,7 @@ contract MinterDALinV1 is ReentrancyGuard, IFilteredMinterV0 {
     IMinterFilterV0 private immutable minterFilter;
 
     /// minterType for this minter
-    string public constant minterType = "MinterDALinV1";
+    string public constant minterType = "MinterSetPriceV1";
 
     uint256 constant ONE_MILLION = 1_000_000;
 
@@ -53,17 +35,10 @@ contract MinterDALinV1 is ReentrancyGuard, IFilteredMinterV0 {
     mapping(uint256 => bool) public projectMaxHasBeenInvoked;
     /// projectId => project's maximum number of invocations
     mapping(uint256 => uint256) public projectMaxInvocations;
-    /// Minimum auction length in seconds
-    uint256 public minimumAuctionLengthSeconds = 3600;
-
-    /// projectId => auction parameters
-    mapping(uint256 => AuctionParameters) public projectAuctionParameters;
-    struct AuctionParameters {
-        uint256 timestampStart;
-        uint256 timestampEnd;
-        uint256 startPrice;
-        uint256 basePrice;
-    }
+    /// projectId => price per token in wei - supersedes any defined core price
+    mapping(uint256 => uint256) private projectIdToPricePerTokenInWei;
+    /// projectId => price per token has been configured on this minter
+    mapping(uint256 => bool) private projectIdToPriceIsConfigured;
 
     modifier onlyCoreWhitelisted() {
         require(
@@ -75,8 +50,8 @@ contract MinterDALinV1 is ReentrancyGuard, IFilteredMinterV0 {
 
     modifier onlyArtist(uint256 _projectId) {
         require(
-            (msg.sender ==
-                genArtCoreContract.projectIdToArtistAddress(_projectId)),
+            msg.sender ==
+                genArtCoreContract.projectIdToArtistAddress(_projectId),
             "Only Artist"
         );
         _;
@@ -88,8 +63,8 @@ contract MinterDALinV1 is ReentrancyGuard, IFilteredMinterV0 {
      * at address `_genArt721Address`.
      * @param _genArt721Address Art Blocks core contract address for
      * which this contract will be a minter.
-     * @param _minterFilter Minter filter for which
-     * this will a filtered minter.
+     * @param _minterFilter Minter filter for whichccthis will a
+     * filtered minter.
      */
     constructor(address _genArt721Address, address _minterFilter)
         ReentrancyGuard()
@@ -140,85 +115,17 @@ contract MinterDALinV1 is ReentrancyGuard, IFilteredMinterV0 {
     }
 
     /**
-     * @notice Sets minimum auction length to `_minimumAuctionLengthSeconds`
-     * for all projects.
-     * @param _minimumAuctionLengthSeconds Minimum auction length in seconds.
+     * @notice Updates this minter's price per token of project `_projectId`
+     * to be '_pricePerTokenInWei`, in Wei.
+     * This price supersedes any legacy core contract price per token value.
      */
-    function setMinimumAuctionLengthSeconds(
-        uint256 _minimumAuctionLengthSeconds
-    ) external onlyCoreWhitelisted {
-        minimumAuctionLengthSeconds = _minimumAuctionLengthSeconds;
-        emit MinimumAuctionLengthSecondsUpdated(_minimumAuctionLengthSeconds);
-    }
-
-    ////// Auction Functions
-    /**
-     * @notice Sets auction details for project `_projectId`.
-     * @param _projectId Project ID to set auction details for.
-     * @param _auctionTimestampStart Timestamp at which to start the auction.
-     * @param _auctionTimestampEnd Timestamp at which to end the auction.
-     * @param _startPrice Price at which to start the auction, in Wei.
-     * @param _basePrice Resting price of the auction, in Wei.
-     */
-    function setAuctionDetails(
+    function updatePricePerTokenInWei(
         uint256 _projectId,
-        uint256 _auctionTimestampStart,
-        uint256 _auctionTimestampEnd,
-        uint256 _startPrice,
-        uint256 _basePrice
+        uint256 _pricePerTokenInWei
     ) external onlyArtist(_projectId) {
-        AuctionParameters memory auctionParams = projectAuctionParameters[
-            _projectId
-        ];
-        require(
-            auctionParams.timestampStart == 0 ||
-                block.timestamp < auctionParams.timestampStart,
-            "No modifications mid-auction"
-        );
-        require(
-            block.timestamp < _auctionTimestampStart,
-            "Only future auctions"
-        );
-        require(
-            _auctionTimestampEnd > _auctionTimestampStart,
-            "Auction end must be greater than auction start"
-        );
-        require(
-            _auctionTimestampEnd >=
-                _auctionTimestampStart + minimumAuctionLengthSeconds,
-            "Auction length must be at least minimumAuctionLengthSeconds"
-        );
-        require(
-            _startPrice > _basePrice,
-            "Auction start price must be greater than auction end price"
-        );
-        projectAuctionParameters[_projectId] = AuctionParameters(
-            _auctionTimestampStart,
-            _auctionTimestampEnd,
-            _startPrice,
-            _basePrice
-        );
-        emit SetAuctionDetails(
-            _projectId,
-            _auctionTimestampStart,
-            _auctionTimestampEnd,
-            _startPrice,
-            _basePrice
-        );
-    }
-
-    /**
-     * @notice Resets auction details for project `_projectId`, zero-ing out all
-     * relevant auction fields. Not intended to be used in normal auction
-     * operation, but rather only in case of the need to halt an auction.
-     * @param _projectId Project ID to set auction details for.
-     */
-    function resetAuctionDetails(uint256 _projectId)
-        external
-        onlyCoreWhitelisted
-    {
-        delete projectAuctionParameters[_projectId];
-        emit ResetAuctionDetails(_projectId);
+        projectIdToPricePerTokenInWei[_projectId] = _pricePerTokenInWei;
+        projectIdToPriceIsConfigured[_projectId] = true;
+        emit PricePerTokenInWeiUpdated(_projectId, _pricePerTokenInWei);
     }
 
     /**
@@ -254,10 +161,14 @@ contract MinterDALinV1 is ReentrancyGuard, IFilteredMinterV0 {
             "Maximum number of invocations reached"
         );
 
-        // _getPrice reverts if auction is unconfigured or has not started
-        uint256 currentPriceInWei = _getPrice(_projectId);
+        // require artist to have configured price of token on this minter
         require(
-            msg.value >= currentPriceInWei,
+            projectIdToPriceIsConfigured[_projectId],
+            "Price not configured"
+        );
+
+        require(
+            msg.value >= projectIdToPricePerTokenInWei[_projectId],
             "Must send minimum value to mint!"
         );
 
@@ -275,7 +186,7 @@ contract MinterDALinV1 is ReentrancyGuard, IFilteredMinterV0 {
         }
 
         // INTERACTIONS
-        _splitFundsETHAuction(_projectId, currentPriceInWei);
+        _splitFundsETH(_projectId);
 
         return tokenId;
     }
@@ -284,22 +195,20 @@ contract MinterDALinV1 is ReentrancyGuard, IFilteredMinterV0 {
      * @dev splits ETH funds between sender (if refund), foundation,
      * artist, and artist's additional payee for a token purchased on
      * project `_projectId`.
-     * @dev utilizes transfer() to send ETH, which may fail if access
-     * lists are not properly populated when purchasing tokens.
-     * @param _projectId Project ID for which funds shall be split.
-     * @param _currentPriceInWei Current price of token, in Wei.
+     * @dev utilizes transfer() to send ETH, so access lists may need to be
+     * populated when purchasing tokens.
      */
-    function _splitFundsETHAuction(
-        uint256 _projectId,
-        uint256 _currentPriceInWei
-    ) internal {
+    function _splitFundsETH(uint256 _projectId) internal {
         if (msg.value > 0) {
-            uint256 refund = msg.value - _currentPriceInWei;
+            uint256 pricePerTokenInWei = projectIdToPricePerTokenInWei[
+                _projectId
+            ];
+            uint256 refund = msg.value - pricePerTokenInWei;
             if (refund > 0) {
                 (bool success_, ) = msg.sender.call{value: refund}("");
                 require(success_, "Refund failed");
             }
-            uint256 foundationAmount = (_currentPriceInWei *
+            uint256 foundationAmount = (pricePerTokenInWei *
                 genArtCoreContract.artblocksPercentage()) / 100;
             if (foundationAmount > 0) {
                 (bool success_, ) = genArtCoreContract.artblocksAddress().call{
@@ -307,7 +216,7 @@ contract MinterDALinV1 is ReentrancyGuard, IFilteredMinterV0 {
                 }("");
                 require(success_, "Foundation payment failed");
             }
-            uint256 projectFunds = _currentPriceInWei - foundationAmount;
+            uint256 projectFunds = pricePerTokenInWei - foundationAmount;
             uint256 additionalPayeeAmount;
             if (
                 genArtCoreContract.projectIdToAdditionalPayeePercentage(
@@ -338,43 +247,14 @@ contract MinterDALinV1 is ReentrancyGuard, IFilteredMinterV0 {
     }
 
     /**
-     * @notice Gets price of minting a token on project `_projectId` given
-     * the project's AuctionParameters and current block timestamp.
-     * Reverts if auction has not yet started or auction is unconfigured.
-     * @param _projectId Project ID to get price of token for.
-     * @return current price of token in Wei
-     */
-    function _getPrice(uint256 _projectId) private view returns (uint256) {
-        AuctionParameters memory auctionParams = projectAuctionParameters[
-            _projectId
-        ];
-        require(
-            block.timestamp > auctionParams.timestampStart,
-            "Auction not yet started"
-        );
-        if (block.timestamp >= auctionParams.timestampEnd) {
-            require(auctionParams.timestampEnd > 0, "Only configured auctions");
-            return auctionParams.basePrice;
-        }
-        uint256 elapsedTime = block.timestamp - auctionParams.timestampStart;
-        uint256 duration = auctionParams.timestampEnd -
-            auctionParams.timestampStart;
-        uint256 startToEndDiff = auctionParams.startPrice -
-            auctionParams.basePrice;
-        return
-            auctionParams.startPrice -
-            ((elapsedTime * startToEndDiff) / duration);
-    }
-
-    /**
      * @notice Gets if price of token is configured, price of minting a
      * token on project `_projectId`, and currency symbol and address to be
      * used as payment. Supersedes any core contract price information.
      * @param _projectId Project ID to get price information for.
-     * @return isConfigured true only if project's auction parameters have been
-     * configured on this minter
+     * @return isConfigured true only if token price has been configured on
+     * this minter
      * @return tokenPriceInWei current price of token on this minter - invalid
-     * if auction has not yet been configured
+     * if price has not yet been configured
      * @return currencySymbol currency symbol for purchases of project on this
      * minter. This minter always returns "ETH"
      * @return currencyAddress currency address for purchases of project on
@@ -390,21 +270,8 @@ contract MinterDALinV1 is ReentrancyGuard, IFilteredMinterV0 {
             address currencyAddress
         )
     {
-        AuctionParameters memory auctionParams = projectAuctionParameters[
-            _projectId
-        ];
-        isConfigured = (auctionParams.startPrice > 0);
-        if (block.timestamp <= auctionParams.timestampStart) {
-            // Provide a reasonable value for `tokenPriceInWei` when it would
-            // otherwise revert, using the starting price before auction starts.
-            tokenPriceInWei = auctionParams.startPrice;
-        } else if (auctionParams.timestampEnd == 0) {
-            // In the case of unconfigured auction, return price of zero when
-            // it would otherwise revert
-            tokenPriceInWei = 0;
-        } else {
-            tokenPriceInWei = _getPrice(_projectId);
-        }
+        isConfigured = projectIdToPriceIsConfigured[_projectId];
+        tokenPriceInWei = projectIdToPricePerTokenInWei[_projectId];
         currencySymbol = "ETH";
         currencyAddress = address(0);
     }
