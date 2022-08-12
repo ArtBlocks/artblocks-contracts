18902a577604752844caa730244a52d19da617f4
ryley-o <30364988+ryley-o@users.noreply.github.com>
2022-08-11 23:04:15 -0700

    update gas tests for gas cost dust changes

--- test/minter-suite-minters/DA/MinterDAExp/MinterDAExpV2.test.ts
+++ test/minter-suite-minters/DA/MinterDAExp/MinterDAExpV2.test.ts
@@ -130,7 +130,7 @@ describe("MinterDAExpV2_V3Core", async function () {
         ethers.utils.formatUnits(txCost, "ether").toString(),
         "ETH"
       );
-      expect(txCost.toString()).to.equal(ethers.utils.parseEther("0.0192677")); // assuming a cost of 100 GWEI
+      expect(txCost.toString()).to.equal(ethers.utils.parseEther("0.0192655")); // assuming a cost of 100 GWEI
     });
   });
 });
