673b3b39758024710ebcdb8e595b6400a89e5cbd
ryley-o <30364988+ryley-o@users.noreply.github.com>
2022-08-15 17:55:26 -0700

    update gas tests

--- test/minter-suite-minters/DA/MinterDAExp/MinterDAExpV2.test.ts
+++ test/minter-suite-minters/DA/MinterDAExp/MinterDAExpV2.test.ts
@@ -130,7 +130,7 @@ describe("MinterDAExpV2_V3Core", async function () {
         ethers.utils.formatUnits(txCost, "ether").toString(),
         "ETH"
       );
-      expect(txCost.toString()).to.equal(ethers.utils.parseEther("0.0192699")); // assuming a cost of 100 GWEI
+      expect(txCost.toString()).to.equal(ethers.utils.parseEther("0.019271")); // assuming a cost of 100 GWEI
     });
   });
 });
--- test/minter-suite-minters/SetPrice/MinterSetPrice/MinterSetPriceV2.test.ts
+++ test/minter-suite-minters/SetPrice/MinterSetPrice/MinterSetPriceV2.test.ts
@@ -158,7 +158,7 @@ describe("MinterSetPriceV2_V3Core", async function () {
         ethers.utils.formatUnits(txCost, "ether").toString(),
         "ETH"
       );
-      expect(txCost.toString()).to.equal(ethers.utils.parseEther("0.0180817")); // assuming a cost of 100 GWEI
+      expect(txCost.toString()).to.equal(ethers.utils.parseEther("0.0180828")); // assuming a cost of 100 GWEI
     });
   });
 
