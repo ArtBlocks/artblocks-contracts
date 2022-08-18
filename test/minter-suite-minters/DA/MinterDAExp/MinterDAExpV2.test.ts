38ac6e745bbb8e5fd65d6e185c213582ba39202a
ryley-o <30364988+ryley-o@users.noreply.github.com>
2022-08-18 12:56:21 -0700

    update gas tests to pass

--- test/minter-suite-minters/DA/MinterDAExp/MinterDAExpV2.test.ts
+++ test/minter-suite-minters/DA/MinterDAExp/MinterDAExpV2.test.ts
@@ -149,7 +149,7 @@ describe("MinterDAExpV2_V3Core", async function () {
         ethers.utils.formatUnits(txCost, "ether").toString(),
         "ETH"
       );
-      expect(txCost.toString()).to.equal(ethers.utils.parseEther("0.016562")); // assuming a cost of 100 GWEI
+      expect(txCost.toString()).to.equal(ethers.utils.parseEther("0.0165046")); // assuming a cost of 100 GWEI
     });
   });
 });
