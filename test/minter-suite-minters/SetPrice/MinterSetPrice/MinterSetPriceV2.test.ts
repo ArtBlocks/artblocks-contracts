4da262c7bc67b854b9f6685645670f995f4f3d9a
ryley-o <30364988+ryley-o@users.noreply.github.com>
2022-08-23 17:49:34 -0700

    Storage pointers on V3 core (#265)
    * properly use storage pointers on V3 core
    
    * update gas tests

--- test/minter-suite-minters/SetPrice/MinterSetPrice/MinterSetPriceV2.test.ts
+++ test/minter-suite-minters/SetPrice/MinterSetPrice/MinterSetPriceV2.test.ts
@@ -177,7 +177,7 @@ describe("MinterSetPriceV2_V3Core", async function () {
         ethers.utils.formatUnits(txCost, "ether").toString(),
         "ETH"
       );
-      expect(txCost.toString()).to.equal(ethers.utils.parseEther("0.0131466")); // assuming a cost of 100 GWEI
+      expect(txCost.toString()).to.equal(ethers.utils.parseEther("0.013127")); // assuming a cost of 100 GWEI
     });
   });
 
