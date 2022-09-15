825cdf78ecfa7bd1b803663f11825e49b8e0ca83
ryley-o <30364988+ryley-o@users.noreply.github.com>
2022-09-14 17:13:30 -0700

    update AdminACLV0 to check for ERC165 support of IAdminACLV0 prior to transferring adminACL

--- contracts/AdminACLV0.sol
+++ contracts/AdminACLV0.sol
@@ -4,15 +4,16 @@ pragma solidity ^0.8.0;
 
 import "./interfaces/0.8.x/IAdminACLV0.sol";
 import "@openzeppelin-4.7/contracts/access/Ownable.sol";
+import "@openzeppelin-4.7/contracts/utils/introspection/ERC165.sol";
 
 /**
  * This contract has a single superAdmin that passes all ACL checks. All checks
  * for any other address will return false.
  */
-contract AdminACLV0 is IAdminACLV0 {
+contract AdminACLV0 is IAdminACLV0, ERC165 {
     string public AdminACLType = "AdminACLV0";
 
-    // superAdmin is the only address that passes any and all ACL checks
+    /// superAdmin is the only address that passes any and all ACL checks
     address public superAdmin;
 
     constructor() {
@@ -20,8 +21,7 @@ contract AdminACLV0 is IAdminACLV0 {
     }
 
     /**
-     * @dev Allows superAdmin to call transferOwnership on other contract from
-     * this contract.
+     * @notice Allows superAdmin change the superAdmin address.
      */
     function changeSuperAdmin(address _newSuperAdmin) external {
         require(msg.sender == superAdmin, "Only superAdmin");
@@ -29,19 +29,29 @@ contract AdminACLV0 is IAdminACLV0 {
     }
 
     /**
-     * @dev Allows superAdmin to call transferOwnership on other contract from
-     * this contract.
+     * Calls transferOwnership on other contract from this contract.
+     * This is useful for updating to a new AdminACL contract.
+     * @dev this function is gated to only superAdmin address.
+     * @dev This implementation requires that the new AdminACL contract
+     * broadcasts support of IAdminACLV0 via ERC165 interface detection.
      */
-    function transferOwnershipOn(address _contract, address _newOwner)
+    function transferOwnershipOn(address _contract, address _newAdminACL)
         external
     {
         require(msg.sender == superAdmin, "Only superAdmin");
-        Ownable(_contract).transferOwnership(_newOwner);
+        // ensure new AdminACL contract supports IAdminACLV0
+        require(
+            ERC165(_newAdminACL).supportsInterface(
+                type(IAdminACLV0).interfaceId
+            ),
+            "AdminACLV0: new admin ACL does not support IAdminACLV0"
+        );
+        Ownable(_contract).transferOwnership(_newAdminACL);
     }
 
     /**
-     * @dev Allows superAdmin to call renounceOwnership on other contract from
-     * this contract.
+     * @notice Calls renounceOwnership on other contract from this contract.
+     * @dev this function is gated to only superAdmin address.
      */
     function renounceOwnershipOn(address _contract) external {
         require(msg.sender == superAdmin, "Only superAdmin");
@@ -49,7 +59,8 @@ contract AdminACLV0 is IAdminACLV0 {
     }
 
     /**
-     * @dev Returns true if `_sender` is `superAdmin` for all ACL checks.
+     * @notice Checks if sender `_sender` is allowed to call function with selector
+     * `_selector` on contract `_contract`. Returns true if sender is superAdmin.
      */
     function allowed(
         address _sender,
@@ -57,5 +68,20 @@ contract AdminACLV0 is IAdminACLV0 {
         bytes4 /*_selector*/
     ) external view returns (bool) {
         return superAdmin == _sender;
+    }
+
+    /**
+     * @inheritdoc ERC165
+     */
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC165)
+        returns (bool)
+    {
+        return
+            interfaceId == type(IAdminACLV0).interfaceId ||
+            super.supportsInterface(interfaceId);
     }
 }
--- contracts/interfaces/0.8.x/IAdminACLV0.sol
+++ contracts/interfaces/0.8.x/IAdminACLV0.sol
@@ -4,11 +4,25 @@
 pragma solidity ^0.8.0;
 
 interface IAdminACLV0 {
-    // Type of the Admin ACL contract of the form "AdminACLV0"
+    /// Type of the Admin ACL contract, e.g. "AdminACLV0"
     function AdminACLType() external view returns (string memory);
 
     /**
-     * Checks if sender `_sender` is allowed to call function with selector
+     * @notice Calls transferOwnership on other contract from this contract.
+     * This is useful for updating to a new AdminACL contract.
+     * @dev this function should be gated to only superAdmin-like addresses.
+     */
+    function transferOwnershipOn(address _contract, address _newAdminACL)
+        external;
+
+    /**
+     * @notice Calls renounceOwnership on other contract from this contract.
+     * @dev this function should be gated to only superAdmin-like addresses.
+     */
+    function renounceOwnershipOn(address _contract) external;
+
+    /**
+     * @notice Checks if sender `_sender` is allowed to call function with selector
      * `_selector` on contract `_contract`.
      */
     function allowed(
