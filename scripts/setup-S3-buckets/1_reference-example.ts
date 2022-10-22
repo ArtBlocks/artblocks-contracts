6ac6c5d92969a1ba9b47bdd98311cb7f5fc38476
ryley-o <30364988+ryley-o@users.noreply.github.com>
2022-10-21 18:10:37 -0700

    add aws bucket creation template script (#352)

---
+++ scripts/setup-S3-buckets/1_reference-example.ts
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+// Created By: Art Blocks Inc.
+import { ethers } from "hardhat";
+import { createPBABBucket } from "../util/aws_s3";
+
+/**
+ * This script was created as a reference of how to setup a new S3 bucket for a new contract
+ */
+const tokenName = "Art Blocks Explorations";
+//////////////////////////////////////////////////////////////////////////////
+// CONFIG ENDS HERE
+//////////////////////////////////////////////////////////////////////////////
+
+async function main() {
+  const network = await ethers.provider.getNetwork();
+  const networkName = network.name == "homestead" ? "mainnet" : network.name;
+
+  // Setup S3 bucket.
+  await createPBABBucket(tokenName, networkName);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
