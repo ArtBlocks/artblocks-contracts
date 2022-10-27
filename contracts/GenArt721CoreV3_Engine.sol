618d83b9b669c384b2d7162aa9b7db680cce24fb
Jacob Rockland <jakerockland@gmail.com>
2022-10-27 11:48:54 -0700

    clarify documentation around autoApproveArtistSplitProposals

--- contracts/GenArt721CoreV3_Engine.sol
+++ contracts/GenArt721CoreV3_Engine.sol
@@ -62,7 +62,8 @@ import "./libs/0.8.x/Bytes32Strings.sol";
  *   to the Admin ACL contract, or the artist if the core contract owner has
  *   renounced ownership. Also note that a proposal will be automatically
  *   accepted if the artist only proposes changed payee percentages without
- *   modifying any payee addresses, or is only removing payee addresses.)
+ *   modifying any payee addresses, or is only removing payee addresses, or
+ *   if the global config `autoApproveArtistSplitProposals` is set to `true`.)
  * - toggleProjectIsPaused (note the artist can still mint while paused)
  * - updateProjectSecondaryMarketRoyaltyPercentage (up to
      ARTIST_MAX_SECONDARY_ROYALTY_PERCENTAGE percent)
@@ -787,6 +788,8 @@ contract GenArt721CoreV3_Engine is
      * automatically approved and the new splits will become active immediately.
      * Automatic approval will also be granted if the artist is only removing
      * additional payee addresses, without adding any new ones.
+     * Also note that if `autoApproveArtistSplitProposals` is true, proposals
+     * will always be auto-approved, regardless of what is being changed.
      * Also note that if the artist is proposing sending funds to the zero
      * address, this function will revert and the proposal will not be created.
      * @param _projectId Project ID.
