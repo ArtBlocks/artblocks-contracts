8ce9052bc3c1ab309b86c1214729d992665be2ae
Ryan Ouyang <ryan.ouyang01@gmail.com>
2022-11-02 00:10:23 -0600

    Add events and checks for sign verifier stuff

--- contracts/mock/iyk/SignVerifierRegistryMock.sol
+++ contracts/mock/iyk/SignVerifierRegistryMock.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract SignVerifierRegistryMock {
+import "./ISignVerifierRegistryMock.sol";
+import "@openzeppelin-4.5/contracts/utils/introspection/ERC165.sol";
+
+contract SignVerifierRegistryMock is ERC165, ISignVerifierRegistryMock {
     mapping(bytes32 => address) signVerifiers;
 
     constructor() {}
@@ -10,7 +13,23 @@ contract SignVerifierRegistryMock {
         signVerifiers[id] = signVerifier;
     }
 
+    function update(bytes32 id, address signVerifier) public {
+        signVerifiers[id] = signVerifier;
+    }
+
     function get(bytes32 id) public view returns (address) {
         return signVerifiers[id];
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC165)
+        returns (bool)
+    {
+        return
+            interfaceId == type(ISignVerifierRegistryMock).interfaceId ||
+            super.supportsInterface(interfaceId);
     }
 }
