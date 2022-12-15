07e4090235a839e62d2b78b574943007127b657b
ben lyaunzon <lyaunzon.b@gmail.com>
2022-12-15 13:48:08 -0800

    adding some more comments

--- contracts/interfaces/0.8.x/IFilteredMinterV2.sol
+++ contracts/interfaces/0.8.x/IFilteredMinterV2.sol
@@ -11,12 +11,18 @@ pragma solidity ^0.8.0;
  * @author Art Blocks Inc.
  */
 interface IFilteredMinterV2 is IFilteredMinterV1 {
+    /**
+     * @notice Local max invocations for project `_projectId`, tied to core contract `_coreContractAddress`,
+     * updated to `_maxInvocations`.
+     */
     event ManuallySetProjectMaxInvocations(
         uint256 indexed _projectId,
         address indexed _coreContractAddress,
         uint256 _maxInvocations
     );
 
+    // Sets the local max invocations for a given project, checking that the provided max invocations is
+    // less than or equal to the global max invocations for the project set on the core contract.
     function manuallySetProjectMaxInvocations(
         uint256 _projectId,
         uint256 _maxInvocations
