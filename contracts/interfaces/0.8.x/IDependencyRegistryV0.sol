d4884a4409a2146b0d15e98efaccf4040840c759
benyo <yoshiwara.b@gmail.com>
2022-12-15 17:49:15 -0800

    Add critical views to interface

--- contracts/interfaces/0.8.x/IDependencyRegistryV0.sol
+++ contracts/interfaces/0.8.x/IDependencyRegistryV0.sol
@@ -63,4 +63,80 @@ interface IDependencyRegistryV0 {
     );
 
     event DependencyTypeScriptUpdated(bytes32 indexed _dependencyTypeId);
+
+    /**
+     * @notice Returns number of registered dependency types
+     * @return Number of registered dependencies.
+     */
+    function getRegisteredDependencyTypeCount() external view returns (uint256);
+
+    /**
+     * @notice Returns registered depenedency type at index `_index`.
+     * @return Registered dependency at `_index`.
+     */
+    function getRegisteredDependencyTypeAtIndex(uint256 _index)
+        external
+        view
+        returns (string memory);
+
+    /**
+     * @notice Returns details for depedency type `_dependencyTypeId`.
+     * @param _dependencyTypeId Dependency type to be queried.
+     * @return typeAndVersion String representation of `_dependencyTypeId`.
+     * (e.g. "p5js(atSymbol)1.0.0")
+     * @return preferredCDN Preferred CDN URL for dependency type
+     * @return additionalCDNCount Count of additional CDN URLs for dependency type
+     * @return preferredRepository Preferred repository URL for dependency type
+     * @return additionalRepositoryCount Count of additional repository URLs for dependency type
+     * @return referenceWebsite Project website URL for dependency type
+     * @return availableOnChain Whether dependency type is available on chain
+     * @return scriptCount Count of on-chain scripts for dependency type
+     */
+    function getDependencyTypeDetails(bytes32 _dependencyTypeId)
+        external
+        view
+        returns (
+            string memory typeAndVersion,
+            string memory preferredCDN,
+            uint24 additionalCDNCount,
+            string memory preferredRepository,
+            uint24 additionalRepositoryCount,
+            string memory referenceWebsite,
+            bool availableOnChain,
+            uint24 scriptCount
+        );
+
+    /**
+     * @notice Returns address with bytecode containing script for
+     * dependency type `_dependencyTypeIds` at script index `_index`.
+     */
+    function getDependencyTypeScriptBytecodeAddressAtIndex(
+        bytes32 _dependencyTypeId,
+        uint256 _index
+    ) external view returns (address);
+
+    /**
+     * @notice Returns script for dependency type `_dependencyTypeId` at script index `_index`.
+     * @param _dependencyTypeId Dependency type to be queried.
+     * @param _index Index of script to be queried.
+     */
+    function getDependencyTypeScriptAtIndex(
+        bytes32 _dependencyTypeId,
+        uint256 _index
+    ) external view returns (string memory);
+
+    /**
+     * @notice Returns the dependency type for a given project (`projectId`)
+     * on a given core contract (`_contractAddress`). If no override is set,
+     * the core contract is called to retrieve the script type and version as
+     * dependency type. For any contract earlier than v3, that does not have
+     * an override set, this will revert.
+     * @param _contractAddress Core contract address.
+     * @param _projectId Project to return dependency type for.
+     * @return dependencyTypeId Dependency type used by project.
+     */
+    function getDependencyTypeForProject(
+        address _contractAddress,
+        uint256 _projectId
+    ) external view returns (string memory);
 }
