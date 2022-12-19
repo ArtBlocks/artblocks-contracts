20f1f20aa0116da47dc6c2bf45c7a8f61b8f3e2b
ben lyaunzon <lyaunzon.b@gmail.com>
2022-12-19 01:23:35 -0500

    adding additional minter interfaces & minter contracts with manuallySetProjectMaxInvocations functionality

---
+++ contracts/interfaces/0.8.x/IFilteredMinterMerkleV2.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+// Created By: Art Blocks Inc.
+
+import "./IFilteredMinterMerkleV1.sol";
+
+pragma solidity ^0.8.0;
+
+/**
+ * @title This interface extends the IFilteredMinterMerkleV0 interface in order
+ * to add support for configuring and indexing the delegation registry address.
+ * @author Art Blocks Inc.
+ */
+interface IFilteredMinterMerkleV2 is IFilteredMinterMerkleV1 {
+    /**
+     * @notice Local max invocations for project `_projectId`, tied to core contract `_coreContractAddress`,
+     * updated to `_maxInvocations`.
+     */
+    event ManuallySetProjectMaxInvocations(
+        uint256 indexed _projectId,
+        address indexed _coreContractAddress,
+        uint256 _maxInvocations
+    );
+
+    // Sets the local max invocations for a given project, checking that the provided max invocations is
+    // less than or equal to the global max invocations for the project set on the core contract.
+    function manuallySetProjectMaxInvocations(
+        uint256 _projectId,
+        uint256 _maxInvocations
+    ) external;
+}
