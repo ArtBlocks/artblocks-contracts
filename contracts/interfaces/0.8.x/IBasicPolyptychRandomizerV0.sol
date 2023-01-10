01f1ecf398f215f7288059195fae956b51de2193
Shantanu Bala <shantanu@sbala.org>
2023-01-10 15:14:21 -0500

    add event

--- contracts/interfaces/0.8.x/IBasicPolyptychRandomizerV0.sol
+++ contracts/interfaces/0.8.x/IBasicPolyptychRandomizerV0.sol
@@ -4,5 +4,13 @@
 pragma solidity 0.8.17;
 
 interface IBasicPolyptychRandomizerV0 is IRandomizerV2 {
+    /**
+     * @notice Minter contract at `_contractAddress` allowed to assign token hash seeds.
+     */
+    event HashSeedSetterUpdated(address indexed _contractAddress);
+
+    /**
+     * @notice Store the token hash seed for an existing token to be re-used in a polyptych panel.
+     */
     function setPolyptychHashSeed(uint256 _tokenId, bytes12 _hashSeed) external;
 }
