95f88067efbfd2b8e07e22283d59888170a5872d
ryley-o <30364988+ryley-o@users.noreply.github.com>
2023-01-25 10:57:31 -0800

    minor refactor

--- gh-action.config.ts
+++ gh-action.config.ts
@@ -1,33 +1,4 @@
+import { solidityConfig } from "./hardhat.solidity-config";
 module.exports = {
-  solidity: {
-    compilers: [
-      {
-        version: "0.5.17",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 100,
-          },
-        },
-      },
-      {
-        version: "0.8.9",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 25,
-          },
-        },
-      },
-      {
-        version: "0.8.17",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 25,
-          },
-        },
-      },
-    ],
-  },
+  solidity: solidityConfig,
 };
