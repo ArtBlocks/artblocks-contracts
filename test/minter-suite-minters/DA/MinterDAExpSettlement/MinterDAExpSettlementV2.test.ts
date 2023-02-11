c025c42da9fac574b09624c5bc7b86c755daa9f6
ryley-o <30364988+ryley-o@users.noreply.github.com>
2023-02-10 21:59:44 -0800

    test for weird edge case

--- test/minter-suite-minters/DA/MinterDAExpSettlement/MinterDAExpSettlementV2.test.ts
+++ test/minter-suite-minters/DA/MinterDAExpSettlement/MinterDAExpSettlementV2.test.ts
@@ -989,6 +989,52 @@ for (const coreContractName of coreContractsToTest) {
       });
     });
 
+    describe("purchase", async function () {
+      it("does not allow purchases even if local max invocations value is returning a false negative", async function () {
+        // set local max invocations to 1
+        await this.minter
+          .connect(this.accounts.artist)
+          .manuallyLimitProjectMaxInvocations(this.projectZero, 1);
+        // switch to different minter
+        const setPriceFactory = await ethers.getContractFactory(
+          "MinterSetPriceV4"
+        );
+        const setPriceMinter = await setPriceFactory.deploy(
+          this.genArt721Core.address,
+          this.minterFilter.address
+        );
+        await this.minterFilter.addApprovedMinter(setPriceMinter.address);
+        await this.minterFilter
+          .connect(this.accounts.artist)
+          .setMinterForProject(0, setPriceMinter.address);
+        // purchase a token on the new minter
+        await setPriceMinter
+          .connect(this.accounts.artist)
+          .updatePricePerTokenInWei(
+            this.projectZero,
+            ethers.utils.parseEther("0")
+          );
+        await setPriceMinter
+          .connect(this.accounts.artist)
+          .purchase(this.projectZero);
+        // switch back to original minter
+        await this.minterFilter
+          .connect(this.accounts.artist)
+          .setMinterForProject(0, this.minter.address);
+        // purchase a token on the original minter
+        // advance to start of auction
+        await ethers.provider.send("evm_mine", [
+          this.startTime + this.auctionStartTimeOffset,
+        ]);
+        await expectRevert(
+          this.minter.connect(this.accounts.user).purchase(this.projectZero, {
+            value: this.startingPrice,
+          }),
+          "Maximum number of invocations reached"
+        );
+      });
+    });
+
     describe("calculate gas", async function () {
       it("mints and calculates gas values [ @skip-on-coverage ]", async function () {
         await ethers.provider.send("evm_mine", [
