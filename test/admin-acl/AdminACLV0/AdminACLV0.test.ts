ac05d42e3cd60835d404f9f351df54bdb0d2c4ec
ryley-o <30364988+ryley-o@users.noreply.github.com>
2023-02-22 17:46:28 -0800

    refactor admin-acl tests

--- test/admin-acl/AdminACLV0/AdminACLV0.test.ts
+++ test/admin-acl/AdminACLV0/AdminACLV0.test.ts
@@ -1,4 +1,5 @@
 import {
+  T_Config,
   getAccounts,
   assignDefaultConstants,
   deployAndGet,
@@ -6,51 +7,54 @@ import {
   mintProjectUntilRemaining,
   advanceEVMByTime,
 } from "../../util/common";
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 
 import { AdminACLV0V1_Common } from "../AdminACLV0V1.common";
 
 /**
  * Tests for functionality of AdminACLV0.
  */
-describe("AdminACLV0", async function () {
-  beforeEach(async function () {
-    // standard accounts and constants
-    this.accounts = await getAccounts();
-    await assignDefaultConstants.call(this);
+describe(`AdminACLV0`, async function () {
+  async function _beforeEach() {
+    let config: T_Config = {
+      accounts: await getAccounts(),
+    };
+    config = await assignDefaultConstants(config);
 
     // deploy and configure minter filter and minter
     ({
-      genArt721Core: this.genArt721Core,
-      minterFilter: this.minterFilter,
-      randomizer: this.randomizer,
-      adminACL: this.adminACL,
-    } = await deployCoreWithMinterFilter.call(
-      this,
+      genArt721Core: config.genArt721Core,
+      minterFilter: config.minterFilter,
+      randomizer: config.randomizer,
+      adminACL: config.adminACL,
+    } = await deployCoreWithMinterFilter(
+      config,
       "GenArt721CoreV3",
       "MinterFilterV1"
     ));
 
-    this.minter = await deployAndGet.call(this, "MinterSetPriceV2", [
-      this.genArt721Core.address,
-      this.minterFilter.address,
+    config.minter = await deployAndGet(config, "MinterSetPriceV2", [
+      config.genArt721Core.address,
+      config.minterFilter.address,
     ]);
 
     // deploy alternate admin ACL that does not broadcast support of IAdminACLV0
-    this.adminACL_NoInterfaceBroadcast = await deployAndGet.call(
-      this,
+    config.adminACL_NoInterfaceBroadcast = await deployAndGet(
+      config,
       "MockAdminACLV0Events",
       []
     );
 
     // deploy another admin ACL that does broadcast support of IAdminACLV0
-    this.adminACL_InterfaceBroadcast = await deployAndGet.call(
-      this,
+    config.adminACL_InterfaceBroadcast = await deployAndGet(
+      config,
       "AdminACLV0",
       []
     );
-  });
+    return config;
+  }
 
-  describe("common tests", async function () {
-    await AdminACLV0V1_Common("AdminACLV0");
+  describe(`common tests`, async function () {
+    await AdminACLV0V1_Common(_beforeEach, "AdminACLV0");
   });
 });
