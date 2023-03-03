fa6fa4294ff105037b94c2b1269eabfb052e10e7
ryley-o <30364988+ryley-o@users.noreply.github.com>
2023-03-03 15:48:08 -0800

    bug fix an import

--- contracts/mock/RandomizerV2_NoAssignMock.sol
+++ contracts/mock/RandomizerV2_NoAssignMock.sol
@@ -3,7 +3,7 @@
 
 pragma solidity 0.8.17;
 
-import "../randomizer/BasicRandomizerV2.sol";
+import "../randomizer/RandomizerV2.sol";
 
 /// @title RandomizerV2_NoAssignMock
 /// @notice WARNING - This is a mock contract. Do not use it in production.
