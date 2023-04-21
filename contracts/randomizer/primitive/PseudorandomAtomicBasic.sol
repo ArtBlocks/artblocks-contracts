84f61c16d9caa0eb51a35036ffa0739560ce5e50
ryley-o <30364988+ryley-o@users.noreply.github.com>
2023-04-21 14:39:43 -0700

    add shared randomizer contracts

---
+++ contracts/randomizer/primitive/PseudorandomAtomicBasic.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+// Creatd By: Art Blocks Inc.
+
+pragma solidity 0.8.17;
+
+import "../../interfaces/0.8.x/IPseudorandomAtomic.sol";
+
+/**
+ * @title PseudorandomAtomic
+ * @author Art Blocks Inc.
+ * @notice This contract atomically returns a pseudorandom value.
+ */
+contract PseudorandomAtomic {
+    // base class that returns
+    function getPseudorandomAtomic(
+        bytes32 _entropy
+    ) external view returns (bytes32) {
+        uint256 time = block.timestamp;
+        return
+            keccak256(
+                abi.encodePacked(
+                    _entropy,
+                    block.number,
+                    blockhash(block.number - 1),
+                    time,
+                    (time % 200) + 1
+                )
+            );
+    }
+}
