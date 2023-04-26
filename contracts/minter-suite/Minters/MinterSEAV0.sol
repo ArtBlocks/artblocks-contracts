5729eb77625fe146f537724672454edfb5b032cf
ryley-o <30364988+ryley-o@users.noreply.github.com>
2023-04-25 17:05:29 -0700

    add stack diagram, better comments

--- contracts/minter-suite/Minters/MinterSEAV0.sol
+++ contracts/minter-suite/Minters/MinterSEAV0.sol
@@ -1113,7 +1113,9 @@ contract MinterSEAV0 is ReentrancyGuard, MinterBase, IFilteredMinterSEAV0 {
      * @param _to The address to send ETH to.
      * @param _amount The amount of ETH to send.
      * @dev This function is adapted from the `forceSafeTransferETH` function
-     * in the `https://github.com/Vectorized/solady` repository.
+     * in the `https://github.com/Vectorized/solady` repository, with
+     * modifications to not check if the current contract has sufficient
+     * balance.
      */
     function _forceSafeTransferETH(address _to, uint256 _amount) internal {
         // load state variable into memory for use in inline assembly
@@ -1127,14 +1129,32 @@ contract MinterSEAV0 is ReentrancyGuard, MinterBase, IFilteredMinterSEAV0 {
 
             // Transfer the ETH and check if it succeeded or not.
             if iszero(call(minterRefundGasLimit_, _to, _amount, 0, 0, 0, 0)) {
-                mstore(0x00, _to) // Store the address in scratch space.
-                mstore8(0x0b, 0x73) // Opcode `PUSH20`.
-                mstore8(0x20, 0xff) // Opcode `SELFDESTRUCT`.
-                // We can directly use `SELFDESTRUCT` in the contract creation.
-                // Compatible with `SENDALL`:
+                // if the transfer failed, we create a temporary contract with
+                // initialization code that uses `SELFDESTRUCT` to force send
+                // the ETH.
+                // note: Compatible with `SENDALL`:
                 // https://eips.ethereum.org/EIPS/eip-4758
+
+                //---------------------------------------------------------------------------------------------------------------//
+                // Opcode  | Opcode + Arguments  | Description  | Stack View                                                     //
+                //---------------------------------------------------------------------------------------------------------------//
+                // Contract creation code that uses `SELFDESTRUCT` to force send ETH to a specified address.                     //
+                // Creation code summary: 0x73<20-byte toAddress>0xff                                                            //
+                //---------------------------------------------------------------------------------------------------------------//
+                // 0x73    |  0x73_toAddress     | PUSH20 toAddress   | toAddress                                                //
+                // 0xFF    |  0xFF               | SELFDESTRUCT       |                                                          //
+                //---------------------------------------------------------------------------------------------------------------//
+                // Store the address in scratch space, starting at 0x00, which begins the 20-byte address at 32-20=12 in memory
+                // @dev use scratch space because we have enough space for simple creation code (less than 0x40 bytes)
+                mstore(0x00, _to)
+                // store opcode PUSH20 immediately before the address, starting at 0x0b (11) in memory
+                mstore8(0x0b, 0x73)
+                // store opcode SELFDESTRUCT immediately after the address, starting at 0x20 (32) in memory
+                mstore8(0x20, 0xff)
+                // this will always succeed because the contract creation code is
+                // valid, and the address is valid because it is a 20-byte value
                 if iszero(create(_amount, 0x0b, 0x16)) {
-                    // For better gas estimation.
+                    // @dev For better gas estimation.
                     if iszero(gt(gas(), 1000000)) {
                         revert(0, 0)
                     }
