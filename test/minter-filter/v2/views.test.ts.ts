da40a7daa6ff12c9808ed5511a8a322219c05eb8
ryley-o <30364988+ryley-o@users.noreply.github.com>
2023-05-12 10:21:59 -0700

    add MinterFilterV2 views tests

--- test/minter-filter/v2/views.test.ts.ts
+++ test/minter-filter/v2/views.test.ts.ts
@@ -1,17 +1,12 @@
-import { BN, constants, expectRevert } from "@openzeppelin/test-helpers";
+import { constants, expectRevert } from "@openzeppelin/test-helpers";
 import { expect } from "chai";
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-import { setupConfigWitMinterFilterV2 } from "../../util/fixtures";
-import {
-  T_Config,
-  getAccounts,
-  assignDefaultConstants,
-  deployAndGet,
-  deployCore,
-  safeAddProject,
-} from "../../util/common";
+import { Contract } from "ethers";
+import { setupConfigWitMinterFilterV2Suite } from "../../util/fixtures";
+import { deployAndGet, deployCore, safeAddProject } from "../../util/common";
 
-// Note: will deploy with compatible minter(s) when they become available
+// we use a dummy shared minter for these tests
+const expectedMinterType = "DummyMinter";
 
 const runForEach = [
   {
@@ -24,20 +19,43 @@ const runForEach = [
     coreFirstProjectNumber: 0,
     minter: "MinterSetPriceV4",
   },
+  // TODO - test more cores
 ];
+
+// helper functions
+async function deployAndRegisterAdditionalCore(
+  config,
+  coreContractName,
+  addInitialProject
+) {
+  // deploy core contract and register on core registry
+  let newCore: Contract;
+  ({ genArt721Core: newCore } = await deployCore(
+    config,
+    coreContractName,
+    config.coreRegistry
+  ));
+  if (addInitialProject) {
+    await safeAddProject(
+      newCore,
+      config.accounts.deployer,
+      config.accounts.artist2.address
+    );
+  }
+  return { newCore };
+}
 
 runForEach.forEach((params) => {
   describe(`MinterFilterV2 Views w/ core ${params.core} core`, async function () {
     async function _beforeEach() {
       // load minter filter V2 fixture
-      const config = await loadFixture(setupConfigWitMinterFilterV2);
+      const config = await loadFixture(setupConfigWitMinterFilterV2Suite);
       // deploy core contract and register on core registry
       ({
         genArt721Core: config.genArt721Core,
         randomizer: config.randomizer,
         adminACL: config.adminACL,
       } = await deployCore(config, params.core, config.coreRegistry));
-      // @dev TODO - add a mock shared minter here
 
       // Project setup
       await safeAddProject(
@@ -53,30 +71,328 @@ runForEach.forEach((params) => {
       return config;
     }
 
+    ///////////////////////////////////////////////////////////////////////////
+    // PUBLIC STATE VARIABLE GETTER FUNCTIONS
+    ///////////////////////////////////////////////////////////////////////////
+
+    describe("minterFilterVersion", async function () {
+      const expectedMinterFilterVersion = "v2.0.0";
+
+      it("returns expected value", async function () {
+        const config = await loadFixture(_beforeEach);
+        let result = await config.minterFilter.minterFilterVersion();
+        expect(result).to.be.equal(expectedMinterFilterVersion);
+      });
+    });
+
+    describe("minterFilterType", async function () {
+      const expectedMinterFilterType = "MinterFilterV2";
+
+      it("returns expected value", async function () {
+        const config = await loadFixture(_beforeEach);
+        let result = await config.minterFilter.minterFilterType();
+        expect(result).to.be.equal(expectedMinterFilterType);
+      });
+    });
+
+    describe("numProjectsUsingMinter", async function () {
+      it("returns zero when no projects have a minter", async function () {
+        const config = await loadFixture(_beforeEach);
+        let result = await config.minterFilter.numProjectsUsingMinter(
+          config.minter.address
+        );
+        expect(result).to.be.equal(0);
+      });
+
+      it("returns expected value when one project has a minter", async function () {
+        const config = await loadFixture(_beforeEach);
+        // assign minter to project zero
+        await config.minterFilter
+          .connect(config.accounts.deployer)
+          .setMinterForProject(
+            config.projectZero,
+            config.genArt721Core.address,
+            config.minter.address
+          );
+        // expect contract to have one minter
+        let result = await config.minterFilter.numProjectsUsingMinter(
+          config.minter.address
+        );
+        expect(result).to.be.equal(1);
+      });
+    });
+
+    ///////////////////////////////////////////////////////////////////////////
+    // EXTERNAL VIEW FUNCTIONS
+    ///////////////////////////////////////////////////////////////////////////
+
+    describe("getMinterForProject", async function () {
+      // expected revert messages
+      const noMinterAssignedRevertMessage = "No minter assigned";
+
+      it("reverts when project does not have minter", async function () {
+        const config = await loadFixture(_beforeEach);
+        await expectRevert(
+          config.minterFilter.getMinterForProject(
+            config.projectZero,
+            config.genArt721Core.address
+          ),
+          noMinterAssignedRevertMessage
+        );
+      });
+
+      it("returns minter address when project has minter", async function () {
+        const config = await loadFixture(_beforeEach);
+        // initial: expect project zero to not have minter
+        await expectRevert(
+          config.minterFilter.getMinterForProject(
+            config.projectZero,
+            config.genArt721Core.address
+          ),
+          noMinterAssignedRevertMessage
+        );
+        // assign minter to project zero
+        await config.minterFilter
+          .connect(config.accounts.deployer)
+          .setMinterForProject(
+            config.projectZero,
+            config.genArt721Core.address,
+            config.minter.address
+          );
+        // expect project zero to have minter
+        let result = await config.minterFilter.getMinterForProject(
+          config.projectZero,
+          config.genArt721Core.address
+        );
+        expect(result).to.be.equal(config.minter.address);
+      });
+    });
+
     describe("projectHasMinter", async function () {
       it("returns false when project does not have minter", async function () {
         const config = await loadFixture(_beforeEach);
-        let result = await config.minterFilter
-          .connect(config.accounts.deployer)
-          .projectHasMinter(config.projectZero, config.genArt721Core.address);
+        let result = await config.minterFilter.projectHasMinter(
+          config.projectZero,
+          config.genArt721Core.address
+        );
         expect(result).to.be.equal(false);
       });
 
       it("returns true when project has minter", async function () {
         const config = await loadFixture(_beforeEach);
-        // TODO - update after mock shared minter is added
-        // // approve minter and assign minter
-        // await config.minterFilter
-        //   .connect(config.accounts.deployer)
-        //   .addApprovedMinter(config.minter.address);
-        // await config.minterFilter
-        //   .connect(config.accounts.deployer)
-        //   .setMinterForProject(config.projectZero, config.minter.address);
-        // // expect project zero to have minter
-        // let result = await config.minterFilter
-        //   .connect(config.accounts.deployer)
-        //   .projectHasMinter(config.projectZero);
-        // expect(result).to.be.equal(true);
+        // initial: expect project zero to not have minter
+        let result = await config.minterFilter.projectHasMinter(
+          config.projectZero,
+          config.genArt721Core.address
+        );
+        expect(result).to.be.equal(false);
+        // assign minter to project zero
+        await config.minterFilter
+          .connect(config.accounts.deployer)
+          .setMinterForProject(
+            config.projectZero,
+            config.genArt721Core.address,
+            config.minter.address
+          );
+        // expect project zero to have minter
+        result = await config.minterFilter.projectHasMinter(
+          config.projectZero,
+          config.genArt721Core.address
+        );
+        expect(result).to.be.equal(true);
+      });
+    });
+
+    describe("getNumProjectsOnContractWithMinters", async function () {
+      it("returns 0 when no projects have a minter", async function () {
+        const config = await loadFixture(_beforeEach);
+        let result =
+          await config.minterFilter.getNumProjectsOnContractWithMinters(
+            config.genArt721Core.address
+          );
+        expect(result).to.be.equal(0);
+      });
+
+      it("returns expected values when projects have minters", async function () {
+        const config = await loadFixture(_beforeEach);
+        // initial: expect core to have no minters
+        let result =
+          await config.minterFilter.getNumProjectsOnContractWithMinters(
+            config.genArt721Core.address
+          );
+        expect(result).to.be.equal(0);
+        // assign minter to project zero
+        await config.minterFilter
+          .connect(config.accounts.deployer)
+          .setMinterForProject(
+            config.projectZero,
+            config.genArt721Core.address,
+            config.minter.address
+          );
+        // expect contract to have one minter
+        result = await config.minterFilter.getNumProjectsOnContractWithMinters(
+          config.genArt721Core.address
+        );
+        expect(result).to.be.equal(1);
+        // assign minter to project one
+        await config.minterFilter
+          .connect(config.accounts.deployer)
+          .setMinterForProject(
+            config.projectOne,
+            config.genArt721Core.address,
+            config.minter.address
+          );
+        // expect contract to have two minters
+        result = await config.minterFilter.getNumProjectsOnContractWithMinters(
+          config.genArt721Core.address
+        );
+        expect(result).to.be.equal(2);
+      });
+    });
+
+    describe("getProjectAndMinterInfoOnContractAt", async function () {
+      it("reverts when index is out of bounds", async function () {
+        const config = await loadFixture(_beforeEach);
+        // @dev we use unspecified here becasue OpenZeppelin lib does not have a
+        // revert message for out of bounds error
+        await expectRevert.unspecified(
+          config.minterFilter.getProjectAndMinterInfoOnContractAt(
+            config.genArt721Core.address,
+            0
+          )
+        );
+      });
+
+      it("returns expected values when projects have minters", async function () {
+        const config = await loadFixture(_beforeEach);
+        // assign minters to two projects
+        await config.minterFilter
+          .connect(config.accounts.deployer)
+          .setMinterForProject(
+            config.projectZero,
+            config.genArt721Core.address,
+            config.minter.address
+          );
+        await config.minterFilter
+          .connect(config.accounts.deployer)
+          .setMinterForProject(
+            config.projectOne,
+            config.genArt721Core.address,
+            config.minter.address
+          );
+        // expect results at index 0
+        let result =
+          await config.minterFilter.getProjectAndMinterInfoOnContractAt(
+            config.genArt721Core.address,
+            0
+          );
+        expect(result.projectId).to.be.equal(config.projectZero);
+        expect(result.minterAddress).to.be.equal(config.minter.address);
+        expect(result.minterType).to.be.equal(expectedMinterType);
+        // expect results at index 1
+        result = await config.minterFilter.getProjectAndMinterInfoOnContractAt(
+          config.genArt721Core.address,
+          1
+        );
+        expect(result.projectId).to.be.equal(config.projectOne);
+        expect(result.minterAddress).to.be.equal(config.minter.address);
+        expect(result.minterType).to.be.equal(expectedMinterType);
+      });
+    });
+
+    describe("isRegisteredCoreContract", async function () {
+      it("returns false when core contract is not registered", async function () {
+        const config = await loadFixture(_beforeEach);
+        let result = await config.minterFilter.isRegisteredCoreContract(
+          constants.ZERO_ADDRESS
+        );
+        expect(result).to.be.equal(false);
+      });
+
+      it("returns true when core contract is registered", async function () {
+        const config = await loadFixture(_beforeEach);
+        let result = await config.minterFilter.isRegisteredCoreContract(
+          config.genArt721Core.address
+        );
+        expect(result).to.be.equal(true);
+      });
+    });
+
+    describe("getProjectsOnContractUsingMinter", async function () {
+      it("returns empty array when no projects have a minter", async function () {
+        const config = await loadFixture(_beforeEach);
+        let result = await config.minterFilter.getProjectsOnContractUsingMinter(
+          config.genArt721Core.address,
+          config.minter.address
+        );
+        expect(result.length).to.be.equal(0);
+      });
+
+      it("returns expected values when one contract is using minter", async function () {
+        const config = await loadFixture(_beforeEach);
+        // assign minter to project zero
+        await config.minterFilter
+          .connect(config.accounts.deployer)
+          .setMinterForProject(
+            config.projectZero,
+            config.genArt721Core.address,
+            config.minter.address
+          );
+        // expect contract to have one minter
+        let result = await config.minterFilter.getProjectsOnContractUsingMinter(
+          config.genArt721Core.address,
+          config.minter.address
+        );
+        expect(result.length).to.be.equal(1);
+        expect(result[0]).to.be.equal(config.projectZero);
+      });
+
+      it("returns expected values when > 1 contract is using minter", async function () {
+        const config = await loadFixture(_beforeEach);
+        // assign minter to project zero
+        await config.minterFilter
+          .connect(config.accounts.deployer)
+          .setMinterForProject(
+            config.projectZero,
+            config.genArt721Core.address,
+            config.minter.address
+          );
+        // deploy and register a second core contract
+        const { newCore } = await deployAndRegisterAdditionalCore(
+          config,
+          "GenArt721CoreV3",
+          true
+        );
+        // assign minter to (ficticious) project zero on second core contract
+        await config.minterFilter
+          .connect(config.accounts.deployer)
+          .setMinterForProject(
+            config.projectZero,
+            newCore.address,
+            config.minter.address
+          );
+        // expect original contract to still only have one project using minter
+        let result = await config.minterFilter.getProjectsOnContractUsingMinter(
+          config.genArt721Core.address,
+          config.minter.address
+        );
+        expect(result.length).to.be.equal(1);
+        expect(result[0]).to.be.equal(config.projectZero);
+        // expect second contract to have one project using minter
+        result = await config.minterFilter.getProjectsOnContractUsingMinter(
+          newCore.address,
+          config.minter.address
+        );
+        expect(result.length).to.be.equal(1);
+        expect(result[0]).to.be.equal(config.projectZero);
+      });
+    });
+
+    describe("owner", async function () {
+      it("returns expected value", async function () {
+        const config = await loadFixture(_beforeEach);
+        let result = await config.minterFilter.owner();
+        expect(result).to.be.equal(config.minterFilterAdminACL.address);
       });
     });
   });
--- test/util/fixtures.ts
+++ test/util/fixtures.ts
@@ -8,8 +8,10 @@ import {
   deployAndGet,
 } from "./common";
 
-// fixture that sets up initial, default config.
-// note that starting project is zero.
+/**
+ * Fixture that sets up initial, default config.
+ * Note: the starting project is set to zero.
+ */
 export async function setupConfig() {
   let config: T_Config = {
     accounts: await getAccounts(),
@@ -19,6 +21,10 @@ export async function setupConfig() {
   return config;
 }
 
+/**
+ * Fixture that sets up initial, default config, with minter filter V2
+ * Note: does not deploy a shared minter.
+ */
 export async function setupConfigWitMinterFilterV2() {
   const config = await loadFixture(setupConfig);
   // deploy minter filter V2
@@ -27,5 +33,20 @@ export async function setupConfigWitMinterFilterV2() {
     minterFilterAdminACL: config.minterFilterAdminACL,
     coreRegistry: config.coreRegistry,
   } = await deploySharedMinterFilter(config, "MinterFilterV2"));
+  return config;
+}
+
+/**
+ * Fixture that sets up initial, default config, with minter filter V2
+ * and deploys a dummy shared minter that is allowlisted on the minter filter
+ */
+export async function setupConfigWitMinterFilterV2Suite() {
+  const config = await loadFixture(setupConfigWitMinterFilterV2);
+  // deploy dummy shared minter
+  config.minter = await deployAndGet(config, "DummySharedMinter", [
+    config.minterFilter.address,
+  ]);
+  // allowlist dummy shared minter on minter filter
+  await config.minterFilter.approveMinterGlobally(config.minter.address);
   return config;
 }
