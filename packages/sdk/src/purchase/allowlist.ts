cc13218b27a6176472793d028b690a73321c765f
Shantanu Bala <shantanu@sbala.org>
2023-05-26 09:28:43 -0500

    adjust allowlist handling

--- packages/sdk/src/purchase/allowlist.ts
+++ packages/sdk/src/purchase/allowlist.ts
@@ -2,6 +2,20 @@ import { MerkleTree } from "merkletreejs";
 import { keccak256, solidityKeccak256 } from "ethers/lib/utils";
 
 /**
+ * @summary Error thrown when a user is not in the provided allowlist.
+ * @description An empty merkle proof is also a valid merkle proof - this
+ * error is thrown when a user is not in the provided allowlist for a project,
+ * allowing client applications to distinguish between a valid empty merkle
+ * proof and a missing allowlist entry.
+ */
+export class AllowlistEntryDoesNotExist extends Error {
+  constructor() {
+    super("An allowlist entry does not exist for the provided wallet address.");
+    this.name = "AllowlistEntryDoesNotExist";
+  }
+}
+
+/**
  * @summary Generate a merkle proof for a user.
  * @description This function is used to generate a merkle proof for a user. This proof
  * is used to verify that the user is in the allowlist for a given project.
@@ -13,6 +27,10 @@ export const generateUserMerkleProof = (
   addresses: string[],
   userAddress: string
 ): string[] => {
+  if (!addresses.includes(userAddress)) {
+    throw new AllowlistEntryDoesNotExist();
+  }
+
   const merkleTree = new MerkleTree(
     addresses.map((addr) => solidityKeccak256(["address"], [addr])),
     keccak256,
