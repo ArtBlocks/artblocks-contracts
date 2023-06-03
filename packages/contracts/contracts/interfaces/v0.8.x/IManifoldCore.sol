950d55ad1efef4fbf42bca3a59bd9b8e379b3c14
Campion Fellin <campionfellin@gmail.com>
2023-06-02 19:54:24 -0700

    fix: clean up IManifoldCore extension
    Signed-off-by: Campion Fellin <campionfellin@gmail.com>

--- packages/contracts/contracts/interfaces/v0.8.x/IManifoldCore.sol
+++ packages/contracts/contracts/interfaces/v0.8.x/IManifoldCore.sol
@@ -15,48 +15,6 @@ interface IManifoldCore {
     function mintExtension(address to) external returns (uint256);
 
     /**
-     * @dev mint a token. Can only be called by a registered extension.
-     * Returns tokenId minted
-     */
-    function mintExtension(
-        address to,
-        string calldata uri
-    ) external returns (uint256);
-
-    /**
-     * @dev mint a token. Can only be called by a registered extension.
-     * Returns tokenId minted
-     */
-    function mintExtension(address to, uint80 data) external returns (uint256);
-
-    /**
-     * @dev batch mint a token. Can only be called by a registered extension.
-     * Returns tokenIds minted
-     */
-    function mintExtensionBatch(
-        address to,
-        uint16 count
-    ) external returns (uint256[] memory);
-
-    /**
-     * @dev batch mint a token. Can only be called by a registered extension.
-     * Returns tokenId minted
-     */
-    function mintExtensionBatch(
-        address to,
-        string[] calldata uris
-    ) external returns (uint256[] memory);
-
-    /**
-     * @dev batch mint a token. Can only be called by a registered extension.
-     * Returns tokenId minted
-     */
-    function mintExtensionBatch(
-        address to,
-        uint80[] calldata data
-    ) external returns (uint256[] memory);
-
-    /**
      * @dev See {IERC721-ownerOf}.
      */
     function ownerOf(uint256 tokenId) external view returns (address);
