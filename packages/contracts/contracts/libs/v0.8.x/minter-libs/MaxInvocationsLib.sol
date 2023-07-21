28ed8fc46bdeee07bc132e0790815c9e75391f9c
ryley-o <30364988+ryley-o@users.noreply.github.com>
2023-07-21 11:17:32 -0700

    add new getter functions to lib

--- packages/contracts/contracts/libs/v0.8.x/minter-libs/MaxInvocationsLib.sol
+++ packages/contracts/contracts/libs/v0.8.x/minter-libs/MaxInvocationsLib.sol
@@ -212,4 +212,26 @@ library MaxInvocationsLib {
             _projectId
         );
     }
+
+    /**
+     * Returns the max invocations for a given MaxInvocationsProjectConfig.
+     * @param _maxInvocationsProjectConfig MaxInvocationsProjectConfig struct
+     * to be queried.
+     */
+    function getMaxInvocations(
+        MaxInvocationsProjectConfig storage _maxInvocationsProjectConfig
+    ) internal view returns (uint256) {
+        return _maxInvocationsProjectConfig.maxInvocations;
+    }
+
+    /**
+     * Returns if max has been invoked for a given MaxInvocationsProjectConfig.
+     * @param _maxInvocationsProjectConfig MaxInvocationsProjectConfig struct
+     * to be queried.
+     */
+    function getMaxHasBeenInvoked(
+        MaxInvocationsProjectConfig storage _maxInvocationsProjectConfig
+    ) internal view returns (bool) {
+        return _maxInvocationsProjectConfig.maxHasBeenInvoked;
+    }
 }
