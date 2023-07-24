64e63d79ef6ec97e31aa2fd8ad29d44fa3b7c609
ryley-o <30364988+ryley-o@users.noreply.github.com>
2023-07-24 16:19:21 -0700

    add scripts and dev deployment logs for shared minters

---
+++ packages/contracts/deployments/minters/goerli-dev/shared-minter-deploy-config.dev.ts
@@ -0,0 +1,56 @@
+// This file is used to configure the deployment of shared minter contracts.
+// It is intended to be imported by the generic shared minter deployer by running
+// one of the commands similar to `deploy:shared-minters:<network>.
+
+export const deployConfigDetailsArray = [
+  {
+    network: "goerli",
+    // environment is only used for metadata purposes, and is not used in the deployment process
+    // Please set to "dev", "staging", or "mainnet", as appropriate
+    environment: "dev",
+    minterName: "MinterSetPriceV5",
+    minterFilterAddress: "0xf4AC1A3904A1ED2c9E969666A24541bB6e4Cd39f",
+    // may only set to true if deploying from the MinterFilter's admin wallet
+    approveMinterGlobally: true,
+  },
+  {
+    network: "goerli",
+    // environment is only used for metadata purposes, and is not used in the deployment process
+    // Please set to "dev", "staging", or "mainnet", as appropriate
+    environment: "dev",
+    minterName: "MinterSetPriceERC20V5",
+    minterFilterAddress: "0xf4AC1A3904A1ED2c9E969666A24541bB6e4Cd39f",
+    // may only set to true if deploying from the MinterFilter's admin wallet
+    approveMinterGlobally: true,
+  },
+  {
+    network: "goerli",
+    // environment is only used for metadata purposes, and is not used in the deployment process
+    // Please set to "dev", "staging", or "mainnet", as appropriate
+    environment: "dev",
+    minterName: "MinterSetPriceMerkleV5",
+    minterFilterAddress: "0xf4AC1A3904A1ED2c9E969666A24541bB6e4Cd39f",
+    // may only set to true if deploying from the MinterFilter's admin wallet
+    approveMinterGlobally: true,
+  },
+  {
+    network: "goerli",
+    // environment is only used for metadata purposes, and is not used in the deployment process
+    // Please set to "dev", "staging", or "mainnet", as appropriate
+    environment: "dev",
+    minterName: "MinterSetPriceHolderV5",
+    minterFilterAddress: "0xf4AC1A3904A1ED2c9E969666A24541bB6e4Cd39f",
+    // may only set to true if deploying from the MinterFilter's admin wallet
+    approveMinterGlobally: true,
+  },
+  {
+    network: "goerli",
+    // environment is only used for metadata purposes, and is not used in the deployment process
+    // Please set to "dev", "staging", or "mainnet", as appropriate
+    environment: "dev",
+    minterName: "MinterSetPricePolyptychV5",
+    minterFilterAddress: "0xf4AC1A3904A1ED2c9E969666A24541bB6e4Cd39f",
+    // may only set to true if deploying from the MinterFilter's admin wallet
+    approveMinterGlobally: true,
+  },
+];
