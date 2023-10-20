defd5140f315b2ae952cb85c1a8ba9e52dc1087b
Lindsay Gilbert <lindsay@artblocks.io>
2023-10-20 16:54:15 -0400

    Update comment describing new parameter

--- packages/contracts/contracts/minter-suite/Minters/MinterSetPriceERC20V5.sol
+++ packages/contracts/contracts/minter-suite/Minters/MinterSetPriceERC20V5.sol
@@ -448,7 +448,7 @@ contract MinterSetPriceERC20V5 is ReentrancyGuard, ISharedMinterV0 {
      * @param to Address to be the new token's owner.
      * @param projectId Project ID to mint a token on.
      * @param coreContract Core contract address for the given project.
-     * @param purchasePricePerToken Price of token in wei sent by purchaser
+     * @param purchasePricePerToken Price of token sent by purchaser
      * @return tokenId Token ID of minted token
      */
     function purchaseTo(
