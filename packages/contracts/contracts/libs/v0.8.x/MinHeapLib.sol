895919d09663c2e619ad27fd03809b0fd705f750
ryley-o <30364988+ryley-o@users.noreply.github.com>
2024-01-12 10:04:31 -0800

    add min heap lib, reach parity with RAMLib for min heap

---
+++ packages/contracts/contracts/libs/v0.8.x/MinHeapLib.sol
@@ -0,0 +1,140 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+// Created By: Art Blocks Inc.
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Library for using uint256 as a mapping to 256 bool values via a bit map.
+ * This is useful for storing a large number of bool values in a compact way.
+ * @dev This implementation is similar to OpenZeppelin's BitMaps library, but a
+ * single uint256 is used directly in memory instead of operating within a
+ * a mapping within a storage struct.
+ * This design limits the number of indices to 256, but is more gas efficient
+ * for use cases that fit within that limit. This is especially true for
+ * operations that require many reads/writes, since SLOAD/STTORE can be managed
+ * outside of the library.
+ */
+library MinHeapLib {
+    struct MinHeap {
+        Node[] heap;
+    }
+
+    struct Node {
+        // value to rank by
+        uint256 value;
+        // identifier associated with entry
+        // @dev typically used to store unrelated metadata in separate mapping
+        bytes32 id;
+    }
+
+    // insert new node into heap
+    function insert(MinHeap storage minHeap, Node memory node) internal {
+        Node[] storage heap = minHeap.heap;
+        // insert value at end of array
+        heap.push(node);
+
+        // bubble up to correct position
+        uint256 currentIndex = heap.length - 1;
+        // @dev int division rounds down, so no need to floor
+        // @dev need to handle zero currentIndex
+        uint256 parentIndex = (currentIndex - 1) / 2;
+        while (
+            currentIndex > 0 &&
+            heap[currentIndex].value < heap[parentIndex].value
+        ) {
+            // swap parent and child
+            // copy parent values to child
+            heap[currentIndex].value = heap[parentIndex].value;
+            heap[currentIndex].id = heap[parentIndex].id;
+            // copy values from child to parent
+            // we know the desired node values are the node values input to this function
+            heap[parentIndex] = node;
+
+            // update indices
+            currentIndex = parentIndex;
+            parentIndex = (currentIndex - 1) / 2;
+        }
+    }
+
+    // remove minimum value (root), return removed node in memory
+    function removeMin(MinHeap storage minHeap) internal returns (Node memory) {
+        Node[] storage heap = minHeap.heap;
+        // base case - heap is empty
+        if (heap.length == 0) {
+            revert("MinHeap: heap is empty");
+        }
+        // remove root node
+        Node memory root = heap[0];
+        // replace root with last node
+        Node storage rootNode = heap[0];
+        rootNode.value = heap[heap.length - 1].value;
+        rootNode.id = heap[heap.length - 1].id;
+        // delete last node
+        heap.pop();
+        // load heap length into memory
+        uint256 heapLength = heap.length;
+
+        // bubble down to correct position
+        uint256 currentIndex = 0;
+        uint256 leftChildIndex = 2 * currentIndex + 1;
+        uint256 rightChildIndex = 2 * currentIndex + 2;
+        // while children still exist and parent is greater than at least one child...
+        while (
+            (leftChildIndex < heapLength &&
+                heap[currentIndex].value > heap[leftChildIndex].value) ||
+            (rightChildIndex < heapLength &&
+                heap[currentIndex].value > heap[rightChildIndex].value)
+        ) {
+            // swap parent and child
+            // copy child values to parent
+            if (
+                // if left child is smaller or right child doesn't exist
+                heap[leftChildIndex].value < heap[rightChildIndex].value ||
+                rightChildIndex >= heap.length
+            ) {
+                // swap with left child
+                // update parent values
+                heap[currentIndex].value = heap[leftChildIndex].value;
+                heap[currentIndex].id = heap[leftChildIndex].id;
+                // update child values
+                // we know the desired node values are the root node values brought to root earlier in this function
+                heap[leftChildIndex] = root;
+                // update current index (child indices are updated below)
+                currentIndex = leftChildIndex;
+            } else {
+                // given prior conditions, right child exists and must be smaller than left child and parent
+                // swap with right child
+                // update parent values
+                heap[currentIndex].value = heap[rightChildIndex].value;
+                heap[currentIndex].id = heap[rightChildIndex].id;
+                // update child values
+                // we know the desired node values are the root node values brought to root earlier in this function
+                heap[rightChildIndex] = root;
+                // update current index (child indices are updated below)
+                currentIndex = rightChildIndex;
+            }
+            // update child indices
+            leftChildIndex = 2 * currentIndex + 1;
+            rightChildIndex = 2 * currentIndex + 2;
+        }
+
+        return root;
+    }
+
+    // getter functions
+
+    // O(1) access to min value (root)
+    function peek(
+        MinHeap storage minHeap
+    ) internal view returns (Node storage) {
+        // min is at index 0
+        return minHeap.heap[0];
+    }
+
+    // O(1) access to num elements
+    function numElements(
+        MinHeap storage minHeap
+    ) internal view returns (uint256) {
+        return minHeap.heap.length;
+    }
+}
---
+++ packages/contracts/contracts/libs/v0.8.x/minter-libs/RAMLibMinheap.sol
@@ -0,0 +1,164 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+// Created By: Art Blocks Inc.
+
+pragma solidity ^0.8.0;
+
+import {IMinterFilterV1} from "../../../interfaces/v0.8.x/IMinterFilterV1.sol";
+
+import {ABHelpers} from "../ABHelpers.sol";
+import {SplitFundsLib} from "./SplitFundsLib.sol";
+import {MaxInvocationsLib} from "./MaxInvocationsLib.sol";
+import {MinHeapLib} from "../MinHeapLib.sol";
+
+import {IERC721} from "@openzeppelin-5.0/contracts/token/ERC721/IERC721.sol";
+import {SafeCast} from "@openzeppelin-5.0/contracts/utils/math/SafeCast.sol";
+
+/**
+ * @title Art Blocks RAM Minter Library
+ * @notice This library is designed for the Art Blocks platform. It includes
+ * Structs and functions that help with ranked auction minters.
+ * @author Art Blocks Inc.
+ */
+
+library RAMLib {
+    using SafeCast for uint256;
+    using MinHeapLib for MinHeapLib.MinHeap;
+
+    // position of RAM Lib storage, using a diamond storage pattern
+    // for this library
+    bytes32 constant RAM_LIB_STORAGE_POSITION = keccak256("ramlib.storage");
+
+    // project-specific parameters
+    struct RAMProjectConfig {
+        // min heap to track bids
+        MinHeapLib.MinHeap minHeap;
+        // mapping of bid id to bid
+        // @dev TODO could be moved outside of project config if deconflicted, and if useful?
+        // @dev TODO - probably make this deterministic and queriable for niave lookup of wallet's bids
+        mapping(bytes32 id => Bid bid) bids;
+        // --- auction parameters ---
+        // @dev max uint24 is 16,777,215 > 1_000_000 max project size
+        uint24 maxActiveBids;
+        // TODO start time, end time, min bid, bid spacing, etc.
+        // --- auction state ---
+        // track number of bids settled to determine if all bids have been settled
+        uint24 numBidsSettled;
+        bool allWinningBidsSettled; // default false
+    }
+
+    struct Bid {
+        uint256 amount;
+        address bidder;
+        bool isSettled; // TODO: could remove this if amount is changed when settling
+        bool isMinted; // TODO: determine if this is needed
+    }
+
+    // Diamond storage pattern is used in this library
+    struct RAMLibStorage {
+        mapping(address coreContract => mapping(uint256 projectId => RAMProjectConfig)) RAMProjectConfigs;
+    }
+
+    function getMinBid(
+        uint256 projectId,
+        address coreContract
+    ) internal view returns (Bid storage minBid) {
+        // load project config
+        RAMProjectConfig storage RAMProjectConfig_ = getRAMProjectConfig({
+            projectId: projectId,
+            coreContract: coreContract
+        });
+        // get the minimum bid ID
+        bytes32 minBidId = RAMProjectConfig_.minHeap.peek().id;
+        // get the bid
+        minBid = RAMProjectConfig_.bids[minBidId];
+    }
+
+    // TODO must limit to only active auctions, etc.
+    function placeBid(
+        uint256 projectId,
+        address coreContract,
+        uint256 bidValue,
+        address bidder,
+        uint24 bidderProjectNonce,
+        uint256 minBidPercentIncrease,
+        uint256 minterRefundGasLimit
+    ) internal {
+        // load project config
+        RAMProjectConfig storage RAMProjectConfig_ = getRAMProjectConfig({
+            projectId: projectId,
+            coreContract: coreContract
+        });
+        // determine if have reached max bids
+        bool reachedMaxBids = RAMProjectConfig_.minHeap.numElements() ==
+            RAMProjectConfig_.maxActiveBids;
+        if (reachedMaxBids) {
+            // remove + refund the minimum Bid
+            MinHeapLib.Node memory removedMinBid = RAMProjectConfig_
+                .minHeap
+                .removeMin();
+            SplitFundsLib.forceSafeTransferETH({
+                to: RAMProjectConfig_.bids[removedMinBid.id].bidder,
+                amount: removedMinBid.value,
+                minterRefundGasLimit: minterRefundGasLimit
+            });
+            // require new bid value is greater than removed minimum bid
+            // @dev overflow checked automatically in Solidity 0.8
+            require(
+                bidValue > (removedMinBid.value * 100) / minBidPercentIncrease,
+                "Insufficient bid value"
+            );
+        }
+        // insert the new Bid
+        bytes32 bidId = bidIdFromInputs({
+            coreContract: coreContract,
+            projectId: projectId,
+            bidder: bidder,
+            bidderProjectNonce: bidderProjectNonce
+        });
+        RAMProjectConfig_.minHeap.insert(
+            MinHeapLib.Node({value: bidValue, id: bidId})
+        );
+    }
+
+    function bidIdFromInputs(
+        address coreContract,
+        uint256 projectId,
+        address bidder,
+        uint24 bidderProjectNonce
+    ) internal pure returns (bytes32 id) {
+        return
+            keccak256(
+                abi.encodePacked(
+                    coreContract,
+                    projectId,
+                    bidder,
+                    bidderProjectNonce
+                )
+            );
+    }
+
+    /**
+     * Loads the RAMProjectConfig for a given project and core
+     * contract.
+     * @param projectId Project Id to get config for
+     * @param coreContract Core contract address to get config for
+     */
+    function getRAMProjectConfig(
+        uint256 projectId,
+        address coreContract
+    ) internal view returns (RAMProjectConfig storage) {
+        return s().RAMProjectConfigs[coreContract][projectId];
+    }
+
+    /**
+     * @notice Return the storage struct for reading and writing. This library
+     * uses a diamond storage pattern when managing storage.
+     * @return storageStruct The SEALibStorage struct.
+     */
+    function s() internal pure returns (RAMLibStorage storage storageStruct) {
+        bytes32 position = RAM_LIB_STORAGE_POSITION;
+        assembly ("memory-safe") {
+            storageStruct.slot := position
+        }
+    }
+}
