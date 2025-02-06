dcd2dec92d1afd796347c4fc0d90fa9654b52bf8
Lindsay Gilbert <lindsay@artblocks.io>
2025-02-06 16:47:47 -0500

    run post-deployment step

--- packages/contracts/deployments/engine/V3/studio/mainnet/2025-02-06-deployment-config.ts
+++ packages/contracts/deployments/engine/V3/studio/mainnet/2025-02-06-deployment-config.ts
@@ -12,7 +12,8 @@ export const deployNetworkConfiguration = {
   safeAddress: "0x52119BB73Ac8bdbE59aF0EEdFd4E4Ee6887Ed2EA",
   transactionServiceUrl: "https://safe-transaction-mainnet.safe.global",
   // set the transaction hash after successful execution of the batch creation
-  transactionHash: "",
+  transactionHash:
+    "0xa469b7ec686749eee83d1fb7ce020f2d107ea55ccb48b0b9f17986ae835a3344",
 };
 
 export const deployConfigDetailsArray = [
