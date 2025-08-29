2e9d3b3c08cf04224eeb25b64f46a98daa4c145b
ryley-o <30364988+ryley-o@users.noreply.github.com>
2025-08-29 16:23:34 -0700

    SRHooks early proto

---
+++ packages/contracts/contracts/web3call/combined-hooks/SRHooks.sol
@@ -0,0 +1,178 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+// Created By: Art Blocks Inc.
+
+pragma solidity 0.8.22;
+
+import {AbstractPMPAugmentHook} from "../augment-hooks/AbstractPMPAugmentHook.sol";
+import {AbstractPMPConfigureHook} from "../configure-hooks/AbstractPMPConfigureHook.sol";
+
+import {IWeb3Call} from "../../interfaces/v0.8.x/IWeb3Call.sol";
+import {IPMPV0} from "../../interfaces/v0.8.x/IPMPV0.sol";
+import {Strings} from "@openzeppelin-5.0/contracts/utils/Strings.sol";
+import {IERC721} from "@openzeppelin-5.0/contracts/interfaces/IERC721.sol";
+import {IDelegationRegistry as IDelegationRegistryV1} from "../../interfaces/v0.8.x/IDelegationRegistry.sol";
+import {IDelegateRegistry as IDelegationRegistryV2} from "../../interfaces/v0.8.x/IDelegateRegistry.sol";
+import {IPMPConfigureHook} from "../../interfaces/v0.8.x/IPMPConfigureHook.sol";
+import {IPMPAugmentHook} from "../../interfaces/v0.8.x/IPMPAugmentHook.sol";
+
+import {EnumerableSet} from "@openzeppelin-5.0/contracts/utils/structs/EnumerableSet.sol";
+
+/**
+ * @title SRHooks
+ * @author Art Blocks Inc.
+ * @notice This hook verifies ownership of any custom squiggle PostParam setting,
+ * and injects the squiggle's token hash into the token's PMPs if configured.
+ * It supports delegate.xyz V1 and V2, and also allows squiggle #9998 for any address that
+ * inscribed the squiggle Relic contract on eth mainnet.
+ * It also allows resetting the squiggle token back to default #1981.
+ * This hook contract has logic for both the augment and configure hooks.
+ * It reverts if the squiggle token id doesn't pass relic or ownership checks during configuring.
+ * Ownership checks are performed during configuring, keeping provenance history indexable and preventing
+ * effects intuitive during transfers and delegation revocations.
+ * If the squiggle token id is configured to be default #1981, the squiggle PostParams will
+ * be stripped, allowing the owner to effectively "clear" the squiggle-related PostParams back to default.
+ */
+contract SRHooks is AbstractPMPAugmentHook, AbstractPMPConfigureHook {
+    using Strings for uint256;
+
+    address public immutable PMPV0_ADDRESS;
+
+    // @dev Set of token ids that are currently in state SEND
+    EnumerableSet.UintSet private _sendingTokens;
+
+    // @dev per-token Set of token ids that are currently receiving via SEND_TO's receiving token
+    mapping(uint256 => EnumerableSet.UintSet) private _receivingTokensTo;
+
+    // enum for the different possible states of a token's SR configuration
+    enum SRState {
+        SEND,
+        RECEIVE,
+        SEND_TO
+    }
+
+    struct SRConfiguration {
+        // @dev packed storage
+        SRState state; // 1 byte
+        address receiver; // 20 bytes
+    }
+
+    mapping(uint256 => SRConfiguration) private _srConfigurations;
+
+    // key values for SR PMP Keys
+    bytes32 public constant SR_PMP_KEY_STATE = keccak256("State");
+
+    /**
+     * @notice Constructor
+     * @param _pmpV0Address The address of the PMPV0 contract.
+     */
+    constructor(address _pmpV0Address) {
+        PMPV0_ADDRESS = _pmpV0Address;
+        // TODO - emit event with relevant state changes
+    }
+
+    /**
+     * @notice Execution logic to be executed when a token's PMP is configured.
+     * Reverts if the squiggle token id is invalid or the liftOwner does not have access to the squiggle token id.
+     * @dev This hook is executed after the PMP is configured.
+     * @param coreContract The address of the core contract that was configured.
+     * @param tokenId The tokenId of the token that was configured.
+     * @param pmpInput The PMP input that was used to successfully configure the token.
+     */
+    function onTokenPMPConfigure(
+        address coreContract,
+        uint256 tokenId,
+        IPMPV0.PMPInput calldata pmpInput
+    ) external view override {
+        // only allow PMPV0 to call this hook
+        require(msg.sender == PMPV0_ADDRESS, "Only PMPV0 allowed");
+
+        // EXTREMELY MINIMAL AND INCOMPLETE IMPLEMENTATION
+        // (just checking proof of concept)
+
+        // check if input key is SR_PMP_KEY_STATE
+        if (keccak256(bytes(pmpInput.key)) == SR_PMP_KEY_STATE) {
+            // manage state changes
+            // load previous state from this hook's storage
+            SRConfiguration memory previousState = _srConfigurations[tokenId];
+            // load new state from PMP input
+            SRState newState = SRState(pmpInput.configuredValue);
+            // if new state is different from previous state, update sets and update storage
+            if (newState != previousState.state) {
+                // @def flawed comparison logic i think
+                // REVERT PREVIOUS STATE ENUMERABLE MAPPINGS
+                // remove token from sending set if it was in it
+                if (previousState.state == SRState.SEND) {
+                    _sendingTokens.remove(tokenId);
+                }
+                // remove token from token's receiving set if it was in send to state
+                if (previousState.state == SRState.SEND_TO) {
+                    _receivingTokensTo[previousState.receiver].remove(tokenId);
+                }
+
+                // POPULATE NEW STATE ENUMERABLE MAPPINGS
+                // add token to sending set if it is now in send state
+                if (newState == SRState.SEND) {
+                    _sendingTokens.add(tokenId);
+                }
+                // add token to token's receiving set if it is now in send to state
+                // @dev TODO - need a better way to get new receiver address from PMP input
+                if (newState == SRState.SEND_TO) {
+                    _receivingTokensTo[pmpInput.configuredValue].add(tokenId);
+                }
+
+                // UPDATE STORAGE
+                _srConfigurations[tokenId] = SRConfiguration({
+                    state: newState,
+                    receiver: pmpInput.configuredValue // same as above, need better way to get new receiver address from PMP input
+                });
+            }
+        }
+
+        // other PMP keys:
+        // - pfp_image
+    }
+
+    /**
+     * @notice Augment the token parameters for a given token.
+     * Augments the token parameters as described in the contract natspec doc.
+     * @dev This hook is called when a token's PMPs are read.
+     * @dev This must return all desired tokenParams, not just additional data.
+     * @param tokenParams The token parameters for the queried token.
+     * @return augmentedTokenParams The augmented token parameters.
+     */
+    function onTokenPMPReadAugmentation(
+        address /* coreContract */,
+        uint256 /* tokenId */,
+        IWeb3Call.TokenParam[] calldata tokenParams
+    )
+        external
+        view
+        override
+        returns (IWeb3Call.TokenParam[] memory augmentedTokenParams)
+    {
+        // create a new augmentedTokenParams array with maximum length of
+        // input tokenParams + 1 extra element for the squiggle's token hash
+        uint256 originalLength = tokenParams.length;
+        uint256 augmentedMaxLength = originalLength + 1;
+        augmentedTokenParams = new IWeb3Call.TokenParam[](augmentedMaxLength);
+
+        // TODO - implement logic
+
+        // return the augmented tokenParams
+        return augmentedTokenParams;
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    )
+        public
+        view
+        override(AbstractPMPAugmentHook, AbstractPMPConfigureHook)
+        returns (bool)
+    {
+        return
+            interfaceId == type(IPMPAugmentHook).interfaceId ||
+            interfaceId == type(IPMPConfigureHook).interfaceId ||
+            super.supportsInterface(interfaceId);
+    }
+}
