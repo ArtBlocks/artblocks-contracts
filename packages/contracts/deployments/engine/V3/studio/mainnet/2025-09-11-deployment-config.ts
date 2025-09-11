5668860c248deefdf33230bdd1a93f178f0d4821
ryley-o <30364988+ryley-o@users.noreply.github.com>
2025-09-11 13:31:53 -0700

    post-deployment script

--- packages/contracts/deployments/engine/V3/studio/mainnet/2025-09-11-deployment-config.ts
+++ packages/contracts/deployments/engine/V3/studio/mainnet/2025-09-11-deployment-config.ts
@@ -12,7 +12,8 @@ export const deployNetworkConfiguration = {
   safeAddress: "0x52119BB73Ac8bdbE59aF0EEdFd4E4Ee6887Ed2EA",
   transactionServiceUrl: "https://safe-transaction-mainnet.safe.global",
   // set the transaction hash after successful execution of the batch creation
-  transactionHash: "",
+  transactionHash:
+    "0xdf5f1102180e98a290ea3032f838d3766c4928d6f22cb6fd568c3aebeb3f490f",
 };
 
 export const deployConfigDetailsArray = [
